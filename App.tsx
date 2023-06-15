e08c4b34965c7dc08afe3bbbb1e7d89829e3e865
jason <lejason123@gmail.com>
2023-06-15 17:19:51 -0500

    create project structure #2

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Howdy this is she</Text>
       <StatusBar style="auto" />
     </View>
   );
