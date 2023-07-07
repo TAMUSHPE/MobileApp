7127277b7ccced3ab31c9077943575744142105b
jason <lejason123@gmail.com>
2023-07-06 19:39:15 -0500

    add maroon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        maroon: "#800000",
+      },
+    },
   },
   plugins: [],
 };
