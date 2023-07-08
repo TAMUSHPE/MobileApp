097bf29449eb317f7c0dd2666944b84a060991e1
jason <lejason123@gmail.com>
2023-07-07 21:59:12 -0500

    added navigation for members->public profile, fetch points data(#17)

--- src/screens/PublicProfile.tsx
+++ src/screens/PublicProfile.tsx
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { pointsData } from '../components/FetchGoogleSheets';
 
 const PublicProfileScreen = () => {
+    const [value, setValue] = useState();
+    useEffect(() => {
+        let data = async () => {
+            setValue(await pointsData());
+        };
+        data();
+    }, []);
+    if (!value) {
+        return (
+            <ActivityIndicator
+                size="large"
+                animating={true}
+                color="rgba(137,232,207,100)"
+            />
+        );
+    }
+    console.log(value)
     return (
-        <View className='flex-1 items-center justify-center'>
-            <Text className="text-4xl font-bold text-center">Public Profile</Text>
+        <View>
+            <Text>
+                Look at Console for data
+            </Text>
         </View>
     )
 }
