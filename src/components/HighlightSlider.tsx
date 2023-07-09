8ae757ed3addd8d443754e4dc6c5f52645739868
jason <lejason123@gmail.com>
2023-07-08 21:28:26 -0500

    added office hour section, edited home screen styling

--- src/components/HighlightSlider.tsx
+++ src/components/HighlightSlider.tsx
@@ -7,7 +7,7 @@ import { Images } from '../../assets'
 const HighlightSlider = () => {
     return (
         <ScrollView
-            className='flex mx-8 mt-5 py-6 bg-slate-200 rounded-md shadow-md'
+            className='flex mx-8 mt-5 py-6 rounded-xl bg-pale-blue shadow-2xl'
             contentContainerStyle={{
                 paddingHorizontal: 16,
                 // paddingTop: 32,
@@ -18,7 +18,7 @@ const HighlightSlider = () => {
             <View className="relative mr-2">
                 <Image
                     source={Images.CAROUSEL_1}
-                    className="h-48 w-72 rounded"
+                    className="h-48 w-72 rounded mr-4"
                 />
                 <View className='flex mt-4 justify-center items-center text-center'>
                     <Text className='font-bold '>SHPE EVENT 1</Text>
@@ -27,7 +27,7 @@ const HighlightSlider = () => {
             <View className="relative mr-2">
                 <Image
                     source={Images.CAROUSEL_2}
-                    className="h-48 w-72 rounded"
+                    className="h-48 w-72 rounded mr-4"
                 />
                 <View className='flex mt-4 justify-center items-center text-center'>
                     <Text className='font-bold '>SHPE EVENT 2</Text>
@@ -37,7 +37,7 @@ const HighlightSlider = () => {
             <View className="relative mr-2">
                 <Image
                     source={Images.CAROUSEL_3}
-                    className="h-48 w-40 rounded"
+                    className="h-48 w-36 rounded"
                 />
                 <View className='flex mt-4 justify-center items-center text-center'>
                     <Text className='font-bold '>SHPESTER</Text>
@@ -45,15 +45,22 @@ const HighlightSlider = () => {
 
             </View>
             <View className="relative mr-2">
-                <Image
-                    style={{ opacity: 0.3 }}
-                    source={Images.CAROUSEL_3}
-                    className="h-48 w-40 rounded opacity-25"
-                />
-                <View className='absolute'>
-                    <Text className=''> Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus excepturi mollitia, repudiandae illo praesentium porro.</Text>
+                {/* There is no direct way to gray-scale an image without relying on outdated package. This is an alternative method. */}
+                <View>
+                    <Image
+                        source={Images.CAROUSEL_3}
+                        className="h-48 w-36 rounded"
+                        style={{ tintColor: 'gray' }}
+                    />
+                    <Image
+                        source={Images.CAROUSEL_3}
+                        className="absolute h-48 w-36 rounded opacity-25"
+                        style={{ opacity: 0.3 }}
+                    />
+                    <View className='absolute'>
+                        <Text className=''> Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus excepturi mollitia, repudiandae illo praesentium porro.</Text>
+                    </View>
                 </View>
-
 
                 <View className='flex mt-4 justify-center items-center text-center'>
                     <Text className='font-bold'>SHPESTER INFO</Text>
