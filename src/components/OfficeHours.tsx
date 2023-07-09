d157b26c26d91eccdb8c3f1cccb3d59189a020b3
jason <lejason123@gmail.com>
2023-07-08 21:28:26 -0500

    added office hour section, edited home screen styling

--- src/components/OfficeHours.tsx
+++ src/components/OfficeHours.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const OfficeHours = () => {
+    let available = true;
     return (
-        <View>
-            <Text>OfficeHours</Text>
+        <View className='flex mt-10 justify-center items-center border-4 border-pale-blue rounded-lg py-8 mx-8'>
+            <Text className='text-3xl'>Office Hours</Text>
+            <Text className='mt-4'> Monday - Thursday </Text>
+            <Text> Zach 450-P1 | 10 a.m. - 2 p.m. </Text>
+            <TouchableOpacity
+                onPress={() => { }}
+                className="flex justify-center items-center mt-7 bg-dark-navy rounded-lg"
+                activeOpacity={0.7}
+            >
+                <Text className="p-4 text-xl text-white">{available ? "Knock on Wall" : "Unavailable"}</Text>
+            </TouchableOpacity>
         </View>
     )
 }
