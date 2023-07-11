0b61edc01f5e49ca511e2bef9bda446d8f1abf73
LucientZ <koblenzpoisiden@gmail.com>
2023-07-11 15:40:09 -0500

    Fixed issue with public profile screen only displaying 0 points

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -11,7 +11,7 @@ const SettingsScreen = ({ navigation }: SettingsProps) => {
 
     return (
         <View className='flex-1 justify-center items-center'>
-            <Text className="text-4xl font-bold text-center">Settings Page w/ Parm</Text>
+            <Text className="text-4xl font-bold text-center">Settings Page w/ Param</Text>
             <Text className='mt-4 text-2xl font-bold text-center text-green-700'>UserID: {userId}</Text>
         </View>
     )
