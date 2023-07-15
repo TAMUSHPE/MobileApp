be2c8274672bec8027248685409af924a8367ee9
jason <lejason123@gmail.com>
2023-07-15 18:59:45 -0500

    officer hour & init homescreen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,12 +2,15 @@ import { ScrollView } from 'react-native';
 import React from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { HomeStackNavigatorParamList } from '../types/Navigation';
+
 import HighlightSlider from '../components/HighlightSlider';
 import OfficeHours from '../components/OfficeHours';
+import HomeBanner from '../components/HomeBanner';
 
 const HomeScreen = ({ route, navigation }: NativeStackScreenProps<HomeStackNavigatorParamList>) => {
     return (
-        <ScrollView className="flex flex-col bg-white">
+        <ScrollView className="flex flex-col bg-offwhite">
+            <HomeBanner />
             <HighlightSlider />
             <OfficeHours />
         </ScrollView>
