1139bea9bdc3cb2f730d94f3e67c3fadf5ef485e
LucientZ <koblenzpoisiden@gmail.com>
2023-07-17 19:01:52 -0500

    Stubbed out ProfileSetup Screens

--- src/screens/ProfileSetup.tsx
+++ src/screens/ProfileSetup.tsx
@@ -3,39 +3,40 @@ import React, { useState } from 'react';
 import { db, auth } from '../config/firebaseConfig';
 import InteractButton from '../components/InteractButton';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { MainStackNavigatorParamList } from '../types/Navigation';
+import { ProfileSetupNavigatorParamList } from '../types/Navigation';
 
-const ProfileSetup = ({ navigation }: NativeStackScreenProps<MainStackNavigatorParamList>) => {
-    // Hooks
-    const [photoURL, setPhotoURL] = useState<string>("");
-    const [name, setName] = useState<string>("");
-    const [bio, setBio] = useState<string>("");
-    const [major, setMajor] = useState<string>("");
-    const [classYear, setClassYear] = useState<string>("");
-    const [pageIndex, setPageIndex] = useState<number>(0);
-    var committees: Array<string> = [];
-    var userInfo = db.collection("users").doc(auth.currentUser?.uid).get();
+const safeAreaViewStyle = ""
 
+const SetupNameAndBio = ({ navigation }: NativeStackScreenProps<ProfileSetupNavigatorParamList>) => {
     return (
-        <SafeAreaView className='flex-1 items-center justify-between bg-dark-navy'>
-            <View>
-                <InteractButton
-                    label="Screens not implemented. Press this to go to homescreen."
-                    pressFunction={() => navigation.replace("HomeStack")}
-                />
-            </View>
-            <View className='flex-row'>
-                <InteractButton
-                    pressFunction={() => {
-                        setPageIndex(pageIndex + 1);
-                    }}
-                />
-                <InteractButton
-                    pressFunction={() => console.log("stub")}
-                />
-            </View>
+        <SafeAreaView className={safeAreaViewStyle}>
+
         </SafeAreaView>
     );
 };
 
-export default ProfileSetup;
+const SetupProfilePicture = ({ navigation }: NativeStackScreenProps<ProfileSetupNavigatorParamList>) => {
+    return (
+        <SafeAreaView className={safeAreaViewStyle}>
+
+        </SafeAreaView>
+    );
+};
+
+const SetupAcademicInformation = ({ navigation }: NativeStackScreenProps<ProfileSetupNavigatorParamList>) => {
+    return (
+        <SafeAreaView className={safeAreaViewStyle}>
+
+        </SafeAreaView>
+    );
+};
+
+const SetupCommittees = ({ navigation }: NativeStackScreenProps<ProfileSetupNavigatorParamList>) => {
+    return (
+        <SafeAreaView className={safeAreaViewStyle}>
+
+        </SafeAreaView>
+    );
+};
+
+export { SetupNameAndBio, SetupProfilePicture, SetupAcademicInformation, SetupCommittees };
