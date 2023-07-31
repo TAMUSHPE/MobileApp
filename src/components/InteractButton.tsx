04d1a4fc4a58f650f7ee13ebad66353c5b73a14a
LucientZ <koblenzpoisiden@gmail.com>
2023-07-30 22:21:15 -0500

    Removed Guest Sign-In and added google logo to google login button

--- src/components/InteractButton.tsx
+++ src/components/InteractButton.tsx
@@ -1,14 +1,32 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 
-const InteractButton = ({ pressFunction, label, buttonStyle, textStyle, opacity }: { pressFunction: Function, label?: string, buttonStyle?: string, textStyle?: string, opacity?: number }) => {
+const InteractButton = ({ pressFunction, label, buttonStyle, textStyle, iconStyle, opacity, iconURI }: { pressFunction: Function, label?: string, buttonStyle?: string, textStyle?: string, iconStyle?: string, opacity?: number, iconURI?: string }) => {
+    let content;
+    if (iconURI) {
+        content = (
+            <View className='flex-row justify-center items-center'>
+                <Image
+                    className={`${iconStyle ?? "w-6 h-6"}`}
+                    source={{
+                        uri: iconURI
+                    }}
+                />
+                <Text className={`p-2 ${textStyle ?? "text-black"}`}>{label ?? "Interact Button"}</Text>
+            </View>
+        );
+    }
+    else {
+        content = (<Text className={`p-2 ${textStyle ?? "text-black"}`}>{label ?? "Interact Button"}</Text>);
+    }
+
     return (
         <TouchableOpacity
             onPress={() => pressFunction()}
             className={`flex justify-center items-center ${buttonStyle ?? "bg-blue-300"}`}
             activeOpacity={opacity ?? 0.7}
         >
-            <Text className={`p-2 ${textStyle ?? "text-black"}`}>{label ?? "Interact Button"}</Text>
+            {content}
         </TouchableOpacity>
     );
 };
