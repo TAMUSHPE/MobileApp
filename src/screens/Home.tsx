dec52162e227ed3762bdffba125998cbb13ae0a6
jason <lejason123@gmail.com>
2023-08-11 00:28:55 -0500

    remove notifcation page and ask permission in home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,6 +9,8 @@ import OfficeSignIn from '../components/OfficeSignIn';
 import { getUser } from '../api/firebaseUtils';
 import { auth } from '../config/firebaseConfig';
 import { UserContext } from '../context/UserContext';
+import askPermissionNotification from '../helpers/pushNotification';
+
 
 const HomeScreen = () => {
     const [localUser, setLocalUser] = useState<User | undefined>(undefined);
@@ -37,6 +39,8 @@ const HomeScreen = () => {
                 });
         };
         getLocalUser();
+
+        askPermissionNotification();
     }, []);
 
 
