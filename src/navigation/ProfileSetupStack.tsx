42858b2db65426fd88a20748a2d765d75922f1bd
jason <lejason123@gmail.com>
2023-08-11 00:54:17 -0500

    remove pushNotifciationSetupScreen import

--- src/navigation/ProfileSetupStack.tsx
+++ src/navigation/ProfileSetupStack.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
 import { ProfileSetupStackNavigatorParams } from "../types/Navigation";
 import { SetupNameAndBio, SetupProfilePicture, SetupAcademicInformation, SetupCommittees } from "../screens/ProfileSetup";
-import PushNotificationSetup from "../screens/PushNotificationSetup";
 
 
 const ProfileSetupStackNavigator = () => {
@@ -17,7 +16,6 @@ const ProfileSetupStackNavigator = () => {
             <ProfileSetupTabs.Screen name="SetupProfilePicture" component={SetupProfilePicture} />
             <ProfileSetupTabs.Screen name="SetupAcademicInformation" component={SetupAcademicInformation} />
             <ProfileSetupTabs.Screen name="SetupCommittees" component={SetupCommittees} />
-            <ProfileSetupTabs.Screen name="SetupNotification" component={PushNotificationSetup} />
         </ProfileSetupTabs.Navigator>
     );
 };
