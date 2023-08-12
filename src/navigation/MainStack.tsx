4cd86e151d1a35cd832fff4afdba1e8672b95c64
jason <jasonisazn@tamu.edu>
2023-08-11 23:20:08 -0500

    setup admin dashboard (#52)

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -5,6 +5,7 @@ import { MainStackNavigatorParams } from '../types/Navigation';
 // Screens  
 import HomeBottomTabs from "./HomeBottomTabs";
 import SettingsScreen from "../screens/Settings";
+import AdminDashboard from "../screens/AdminDashboard";
 
 const MainStackNavigator = () => {
     const MainStack = createNativeStackNavigator<MainStackNavigatorParams>();
@@ -16,6 +17,7 @@ const MainStackNavigator = () => {
         >
             <MainStack.Screen name="HomeBottomTabs" component={HomeBottomTabs} />
             <MainStack.Screen name="SettingsScreen" component={SettingsScreen} />
+            <MainStack.Screen name="AdminDashboard" component={AdminDashboard} />
         </MainStack.Navigator>
     );
 };
