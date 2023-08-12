709799e70327bde5641880b943796757d2a9e162
jason <lejason123@gmail.com>
2023-08-11 20:38:11 -0500

    added smaller button with link (#19)

--- src/components/ResourceSmallButton.tsx
+++ src/components/ResourceSmallButton.tsx
@@ -1,11 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Linking, TouchableOpacity } from 'react-native'
+import React, { useCallback } from 'react'
+type ResourceSmallButtonProps = {
+    items: {
+        title: string;
+        url: string;
+        image: any;
+        'bg-color': string;
+        'text-color': string;
+    }
+}
 
-const ResourceSmallButton = () => {
+const ResourceSmallButton: React.FC<ResourceSmallButtonProps> = ({ items }) => {
+    const handlePress = useCallback(async () => {
+        const supported = await Linking.canOpenURL(items.url);
+        if (supported) {
+            await Linking.openURL(items.url);
+        } else {
+            console.log(`Don't know how to open this URL: ${items.url}`);
+        }
+    }, [items.url]);
+
     return (
-        <View>
-            <Text>ResourceSmallButton</Text>
-        </View>
+        <TouchableOpacity className='h-36 w-[50%] justify-center items-center mt-6'
+            onPress={() => handlePress()}>
+            <View className="flex w-[80%] justify-center items-center rounded-xl"
+                style={{ backgroundColor: items["bg-color"] }}>
+                <Image
+                    source={items.image}
+                    className="h-[90%] w-[72%] rounded-xl"
+                />
+
+                {/* <Text className={`absolute left-0 bottom-0 ml-4 font-bold text-${items['text-color']}  mb-1 text-2xl`}>{items.title}</Text> */}
+
+            </View>
+        </TouchableOpacity>
     )
 }
 
