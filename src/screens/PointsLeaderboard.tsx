00ce002d4f575b8e9a2412f0f893f3c412859cc4
jason <jasonisazn@tamu.edu>
2023-08-14 03:03:27 -0500

    add current user card

--- src/screens/PointsLeaderboard.tsx
+++ src/screens/PointsLeaderboard.tsx
@@ -5,9 +5,11 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RankChange, ResourcesStackNavigatorParams } from '../types/Navigation';
 import RankCard from '../components/RankCard';
 import { queryGoogleSpreadsheet, GoogleSheetsIDs } from '../api/fetchGoogleSheets'
-import { getPhotoByEmail } from '../api/firebaseUtils'
+import { getUserByEmail } from '../api/firebaseUtils'
 import { Images } from '../../assets';
-import { auth } from '../config/firebaseConfig';
+import { memberPoints } from '../api/fetchGoogleSheets'
+import { auth, db } from "../config/firebaseConfig"
+import { collection, getDocs, where, query, doc, getDoc } from 'firebase/firestore';
 
 type userData = {
     name: string;
@@ -22,9 +24,17 @@ const PointsLeaderboard = ({ navigation }: { navigation: NativeStackNavigationPr
     const debounceTimer = useRef<NodeJS.Timeout | null>(null);
     const [loading, setLoading] = useState(true);
 
+    // userPoints obtained from google sheets
+    // userRank and rankChange are directly obtain from firebase
+    // (this also means that rankChange is only obtain if user exist on firebase)
+    // This method may be changed in the future 
+    const [userPoints, setUserPoints] = useState(-1);
+    const [userRank, setUserRank] = useState(-1);
+    const [userRankChange, setUserRankChange] = useState<RankChange>('same');
 
 
-    const queryAndSetRanks = (limit: number, offset: number) => {
+
+    const queryAndSetRanks = async (limit: number, offset: number) => {
         const query = `select A, B, C, D LIMIT ${limit} OFFSET ${offset}`;
         queryGoogleSpreadsheet(GoogleSheetsIDs.POINTS_ID, query)
             .then(response => {
@@ -46,24 +56,60 @@ const PointsLeaderboard = ({ navigation }: { navigation: NativeStackNavigationPr
     const prepUserData = async (data: any[], offset: number): Promise<userData[]> => {
         const usersDataPromises = data.map(async (entry, index) => {
             const email = entry.c[2].v;
-            const profileURL = await getPhotoByEmail(email);
+            const fetchUser = await getUserByEmail(email);
+            const profileURL = fetchUser?.profileURL;
+            const rankChange = fetchUser?.rankChange;
             return {
                 name: `${entry.c[0].v} ${entry.c[1].v}`,
                 email: email,
                 points: entry.c[3].f,
                 rank: index + offset + 1,
                 image: profileURL ? { uri: profileURL } : null,
-                rankChange: 'same' as RankChange
+                rankChange: rankChange as RankChange,
             };
         });
 
         return await Promise.all(usersDataPromises);
     }
 
+    const colorMapping: Record<RankChange, string> = {
+        "increased": "#AEF359",
+        "same": "#7F7F7F",
+        "decreased": "#B22222"
+    };
+
+    const getUserRank = async () => {
+        const userRef = doc(db, 'users', auth.currentUser?.uid!);
+        const docSnap = await getDoc(userRef);
+
+        if (!docSnap.exists()) {
+            return null;
+        }
+        const userData = docSnap.data();
+        setUserRank(userData?.rank);
+        setUserRankChange(userData?.rankChange);
+    }
+
+    const RenderUserRankChange = () => {
+        switch (userRankChange) {
+            case "increased":
+                return <Octicons name="chevron-up" size={24} color="#AEF359" />;
+            case "decreased":
+                return <Octicons name="chevron-down" size={24} color="#FF0000" />;
+            default:
+                return <Octicons name="dash" size={22} color="gray" />
+        }
+    };
+
 
     useEffect(() => {
+        getUserRank();
         queryAndSetRanks(13, 0);
+        memberPoints(auth?.currentUser?.email!).then(points => {
+            setUserPoints(points);
+        })
     }, [])
+
 
     const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }: NativeScrollEvent) => {
         const paddingToBottom = 20;
@@ -188,18 +234,32 @@ const PointsLeaderboard = ({ navigation }: { navigation: NativeStackNavigationPr
 
                 <View className={`bg-white rounded-t-2xl flex-1 ${rankCards.length === 0 ? 'pb-96' : 'pb-20'}`}>
                     {/* User Ranking */}
-                    <View className='flex-row bg-[#AEF359] h-14 mx-4 px-4 mt-8 rounded-xl items-center '>
-                        <View className='flex-1'>
-                            <Text className='text-xl font-medium'>Your Ranking</Text>
+                    <TouchableOpacity onPress={() => {
+                        navigation.navigate("PublicProfile", { email: "jhernandez18@tamu.edu" });
+                    }}>
+                        <View
+                            className={`flex-row h-20 mx-4 px-4 mt-8 rounded-xl items-center`}
+                            style={{ backgroundColor: colorMapping[userRankChange] }}
+                        >
+                            <View className='flex-1 flex-row items-center'>
+                                <View className='w-[65%]'>
+                                    <Text className='text-xl font-medium'>Your Ranking</Text>
+                                    <View className='flex-row items-center'>
+                                        <Octicons name="star-fill" size={24} color="black" />
+                                        <Text className='text-xl font-medium ml-2'>{userPoints} pts</Text>
+                                    </View>
+
+                                </View>
+                            </View>
+                            <View className='flex-row'>
+                                <Text className='text-xl font-medium mr-4'>{userRank}</Text>
+                                <View className='bg-white h-7 w-7 rounded-full items-center justify-center'>
+                                    <RenderUserRankChange />
+                                </View>
+                            </View>
                         </View>
-                        <View className='flex-row'>
-                            <Text className='text-xl font-medium mr-4'>5</Text>
-                            {/* <View className='bg-white h-7 w-7 rounded-full items-center justify-center'>
-                                <Octicons name="chevron-down" size={24} color="#FF0000" />
-                                <Octicons name="chevron-up" size={24} color="#AEF359" />
-                            </View> */}
-                        </View>
-                    </View>
+                    </TouchableOpacity>
+
                     {/* Leaderboard */}
                     {rankCards.slice(3).map((userData, index) => (
                         <RankCard key={index + 3} userData={userData} navigation={navigation} />
