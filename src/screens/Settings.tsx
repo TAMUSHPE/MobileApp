a18d1a8033b75c429d2365aac86f4dc10f750f14
LucientZ <koblenzpoisiden@gmail.com>
2023-08-23 16:04:01 -0500

    Added Search Screens

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, ScrollView, TextInput, TouchableHighlight } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, TouchableHighlight, TouchableOpacity, SafeAreaView } from 'react-native';
 import React, { useContext } from 'react';
 import { SettingsStackParams } from '../types/Navigation';
 import { Images } from '../../assets';
@@ -13,31 +13,25 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'
  * @param mainText - The large text to be displayed on the button. This should be one or two words briefly explaining what the button does
  * @param mainText - The smaller text to be displayed on the button. This should add more details to what the button does
  */
-const SettingsNavigationButton = ({ iconName, mainText, subText, darkMode, onPress }: { iconName: keyof typeof MaterialCommunityIcons.glyphMap, mainText: string, subText?: string, darkMode?: boolean, onPress: Function }) => {
-    const mainTextClassName = "text-2xl"
-    const textViewClassName = "ml-3 flex-col"
-    const textContent = subText ?
-        (<View className={textViewClassName}>
-            <Text className={mainTextClassName}>{mainText}</Text>
-            <Text className={`text-lg ${darkMode}`}>{subText}</Text>
-        </View>) :
-        (<View className={textViewClassName}>
-            <Text className={mainTextClassName}>{mainText}</Text>
-        </View>);
-
+const SettingsNavigationButton = ({ iconName, mainText, subText, darkMode, onPress }: { iconName?: keyof typeof MaterialCommunityIcons.glyphMap, mainText: string, subText?: string, darkMode?: boolean, onPress?: Function }) => {
     return (
         <TouchableHighlight
-            onPress={() => onPress()}
+            onPress={() => onPress ? onPress() : console.log("Button Pressed")}
             underlayColor="#DDD"
             className='w-full h-24 justify-center px-3'
         >
             <View className='flex-row my-2 items-center'>
-                <MaterialCommunityIcons name={iconName} size={46} color={darkMode ? "white" : "black"} />
-                {textContent}
+                {iconName && <MaterialCommunityIcons name={iconName} size={46} color={darkMode ? "white" : "black"} />}
+                <View className="ml-3 flex-col">
+                    <Text className={"text-2xl"}>{mainText}</Text>
+                    {subText && <Text className={`text-lg ${darkMode}`}>{subText}</Text>}
+                </View>
             </View>
         </TouchableHighlight>
     );
 };
+
+const SettingsToggleButton = ({})
 
 const SettingsScreen = ({ navigation }: NativeStackScreenProps<SettingsStackParams>) => {
     const { userInfo } = useContext(UserContext) ?? {};
@@ -49,15 +43,21 @@ const SettingsScreen = ({ navigation }: NativeStackScreenProps<SettingsStackPara
                 alignItems: 'center'
             }}
         >
-            <View className='w-full py-8 px-3 flex-row justify-end'>
-                <Image
-                    className='w-20 h-20 rounded-full'
-                    defaultSource={Images.DEFAULT_USER_PICTURE}
-                    source={auth?.currentUser?.photoURL ? { uri: auth?.currentUser?.photoURL } : Images.DEFAULT_USER_PICTURE}
-                />
+            <View className='w-full py-8 px-4 flex-row items-center justify-between'>
+                <View>
+                    <Text className='text-4xl'>{userInfo?.publicInfo?.displayName}</Text>
+                    <Text className='text-xl'>{userInfo?.publicInfo?.name}</Text>
+                </View>
+                <TouchableOpacity onPress={() => navigation.navigate("ProfileSettingsScreen")}>
+                    <Image
+                        className='w-24 h-24 rounded-full'
+                        defaultSource={Images.DEFAULT_USER_PICTURE}
+                        source={auth?.currentUser?.photoURL ? { uri: auth?.currentUser?.photoURL } : Images.DEFAULT_USER_PICTURE}
+                    />
+                </TouchableOpacity>
             </View>
             <TouchableHighlight
-                onPress={() => console.log("Search Bar Pressed")}
+                onPress={() => navigation.navigate("SearchSettingsScreen")}
                 className='rounded-full w-11/12 p-4 mb-5 mt-2 bg-[#E9E9E9]'
                 underlayColor={"#ccd3d8"}
             >
@@ -71,34 +71,43 @@ const SettingsScreen = ({ navigation }: NativeStackScreenProps<SettingsStackPara
                 mainText='Profile'
                 subText='Photo, Display Name, etc..'
                 darkMode={userInfo?.private?.privateInfo?.settings?.darkMode}
-                onPress={() => console.log("Account Button Pressed")}
+                onPress={() => navigation.navigate("ProfileSettingsScreen")}
             />
             <SettingsNavigationButton
                 iconName='brightness-6'
                 mainText='Display'
                 subText='Dark/Light theme'
                 darkMode={userInfo?.private?.privateInfo?.settings?.darkMode}
-                onPress={() => console.log("Display Button Pressed")}
+                onPress={() => navigation.navigate("DisplaySettingsScreen")}
             />
             <SettingsNavigationButton
                 iconName='account-box'
                 mainText='Account'
                 subText='Email, Password..'
                 darkMode={userInfo?.private?.privateInfo?.settings?.darkMode}
-                onPress={() => console.log("Account Button Pressed")}
+                onPress={() => navigation.navigate("AccountSettingsScreen")}
             />
             <SettingsNavigationButton
                 iconName='information-outline'
                 mainText='About'
                 subText='Information about the app'
                 darkMode={userInfo?.private?.privateInfo?.settings?.darkMode}
-                onPress={() => console.log("About Button Pressed")}
+                onPress={() => navigation.navigate("AboutScreen")}
             />
         </ScrollView>
     )
 }
 
-const ProfileSettingsScreen = () => {
+const SearchSettingsScreen = ({ navigation }: NativeStackScreenProps<SettingsStackParams>) => {
+    alert("Settings Search and Settings Transition Unimplemented")
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    );
+};
+
+const ProfileSettingsScreen = ({ navigation }: NativeStackScreenProps<SettingsStackParams>) => {
     return (
         <ScrollView>
 
@@ -106,7 +115,7 @@ const ProfileSettingsScreen = () => {
     );
 };
 
-const DisplaySettingsScreen = () => {
+const DisplaySettingsScreen = ({ navigation }: NativeStackScreenProps<SettingsStackParams>) => {
     return (
         <ScrollView>
 
@@ -114,7 +123,7 @@ const DisplaySettingsScreen = () => {
     );
 };
 
-const AccountSettingsScreen = () => {
+const AccountSettingsScreen = ({ navigation }: NativeStackScreenProps<SettingsStackParams>) => {
     return (
         <ScrollView>
 
@@ -122,7 +131,7 @@ const AccountSettingsScreen = () => {
     );
 };
 
-const AboutScreen = () => {
+const AboutScreen = ({ navigation }: NativeStackScreenProps<SettingsStackParams>) => {
     return (
         <ScrollView>
 
@@ -131,5 +140,5 @@ const AboutScreen = () => {
 };
 
 
-export default SettingsScreen;
+export { SettingsScreen, SearchSettingsScreen, ProfileSettingsScreen, DisplaySettingsScreen, AccountSettingsScreen, AboutScreen };
 
