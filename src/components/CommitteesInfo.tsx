da346a6994e78c9ffc855973336de25236bd80eb
jason <jasonisazn@tamu.edu>
2023-08-24 22:43:34 -0500

    most of committees screen, firebase functions, types added (#15)

--- src/components/CommitteesInfo.tsx
+++ src/components/CommitteesInfo.tsx
@@ -1,43 +1,103 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Images } from '../../assets';
+import { Committee } from '../types/Committees';
+import { getCommitteeInfo, getPublicUserData } from '../api/firebaseUtils';
+import { PublicUserInfo } from '../types/User';
 
-const CommitteesInfo = () => {
+const CommitteesInfo: React.FC<CommitteesInfoProp> = ({ selectedCommittee }) => {
+    if (!selectedCommittee) {
+        return null; // replace with a proper empty screen
+    }
+    const [committeeInfo, setCommitteeInfo] = useState<Committee | null>(null);
+    const [headUserInfo, setHeadUserInfo] = useState<PublicUserInfo | null>(null);
+    const [leadsUserInfo, setLeadsUserInfo] = useState<PublicUserInfo[]>([]);
+
+    useEffect(() => {
+        const fetchCommitteeInfo = async () => {
+            setCommitteeInfo(null);
+            setHeadUserInfo(null);
+            setLeadsUserInfo([]);
+
+            if (selectedCommittee.name) {
+                const fetchedInfo = await getCommitteeInfo(selectedCommittee.name);
+                if (fetchedInfo) {
+                    setCommitteeInfo(fetchedInfo);
+                    if (fetchedInfo.headUID) {
+                        fetchHeadUserData(fetchedInfo.headUID);
+                    }
+                    if (fetchedInfo.leadUIDs && fetchedInfo.leadUIDs.length > 0) {
+                        fetchedInfo.leadUIDs.forEach(uid => {
+                            fetchLeadUserData(uid);
+                        })
+                    }
+
+                }
+            }
+        }
+
+        const fetchHeadUserData = async (uid: string) => {
+            const fetchedInfo = await getPublicUserData(uid);
+            if (fetchedInfo) {
+                setHeadUserInfo(fetchedInfo);
+            }
+        };
+
+        const fetchLeadUserData = async (uid: string) => {
+            const fetchedInfo = await getPublicUserData(uid);
+            if (fetchedInfo) {
+                setLeadsUserInfo(prevState => [...prevState, fetchedInfo]);
+            }
+        }
+
+        fetchCommitteeInfo();
+    }, [selectedCommittee.name]);
+
     return (
         <View>
             <View className='flex-row justify-between mx-4 mt-10'>
-                <View className='h-52 w-[40%] shadow-2xl rounded-3xl'
-                    style={{
-                        shadowColor: '#171717',
-                        shadowOffset: { width: -2, height: 4 },
-                        shadowOpacity: 0.2,
-                        shadowRadius: 3,
-                    }}
-                >
-                    <Image source={Images.COMMITTEE_1} className='h-full w-full bg-pale-blue rounded-3xl' />
+                {/* Committee Image */}
+                <View className='h-52 w-[40%] shadow-2xl rounded-3xl'>
+                    <Image source={selectedCommittee?.image || Images.COMMITTEE_4} className='h-full w-full bg-pale-blue rounded-3xl' />
+                    <View className='absolute items-center w-full'>
+                        <Text className='text-lg font-bold'>{selectedCommittee?.name}</Text>
+                    </View>
                 </View>
+
+                {/* Committee Info */}
                 <View className='w-[60%]'>
                     <View className='flex-row justify-between mx-4 items-center'>
                         <View className='w-[1/3] items-center'>
                             <Text>Head</Text>
-                            <Text>p0</Text>
+                            <Image source={headUserInfo?.photoURL ? { uri: headUserInfo?.photoURL } : Images.DEFAULT_USER_PICTURE} className='h-8 w-8 mt-2 rounded-full' />
                         </View>
+
                         <View className='w-[1/3] items-center'>
-                            <Text>Lead</Text>
-                            <View className='flex-row'>
-                                <Text>p1</Text>
-                                <Text>p2</Text>
-                                <Text>p3</Text>
+                            {leadsUserInfo.length > 0 && <Text>Lead</Text>}
+                            <View className='flex-row-reverse mt-2'>
+                                {leadsUserInfo.map((lead, index) => (
+                                    <View className='w-4 '>
+                                        <Image source={leadsUserInfo[index].photoURL ? { uri: leadsUserInfo[index].photoURL } : Images.DEFAULT_USER_PICTURE} className='h-8 w-8 rounded-full' />
+                                    </View>
+                                ))}
+
                             </View>
                         </View>
-                        <View className='w-[1/3] items-center'>
+
+                        <View className='w-[1/3] items-center h-full'>
                             <Text>Members</Text>
-                            <Text>30</Text>
+                            <View className='mt-2'>
+                                <Text className=''>{committeeInfo?.memberCount}</Text>
+                            </View>
                         </View>
+
                     </View>
-                    <Text className='mt-3 mx-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nemo doloribus aliquam commodi soluta tempore harum at odio, obcaecati, perspiciatis veniam. Explicabo repudiandae, magni voluptatem dignissimos quo doloremque consectetur excepturi?</Text>
+
+                    <Text className='mt-3 mx-4'>{committeeInfo?.description}</Text>
+
                 </View>
             </View>
+
             <View className='flex-row mx-4 mt-4 space-x-2'>
                 <TouchableOpacity
                     className='bg-white rounded-xl h-8 w-[8%] items-center justify-center border-gray-600 border'
@@ -51,13 +111,17 @@ const CommitteesInfo = () => {
                 </TouchableOpacity>
                 <TouchableOpacity
                     className='bg-white rounded-xl h-8 w-[43%] items-center justify-center border-gray-600 border'
-
                 >
                     <Text>Leader Application</Text>
                 </TouchableOpacity>
             </View>
+
         </View>
     )
+}
+
+type CommitteesInfoProp = {
+    selectedCommittee: Committee | null;
 }
 
 export default CommitteesInfo
