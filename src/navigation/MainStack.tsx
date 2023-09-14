d3ba1b57b67461f4536234f8b60d4169f3d32959
LucientZ <koblenzpoisiden@gmail.com>
2023-09-14 02:23:48 -0500

    Revert to "Merge branch 'dev' into QRCode" hash f2169ae
    This reverts commit 009003dcc58f4de22e63caa3d69e03fa4793ba37, reversing
    changes made to 2e57ae0c85f8f18f4055044a8a56c4f90bb67193.

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeBottomTabs from "./HomeBottomTabs";
 import AdminDashboard from "../screens/AdminDashboard";
-import EventVerification from "../screens/EventVerification";
 import { MainStackParams } from '../types/Navigation';
 import { SettingsScreen, SearchSettingsScreen, ProfileSettingsScreen, DisplaySettingsScreen, AccountSettingsScreen, AboutSettingsScreen } from "../screens/Settings";
 import { UserContext } from "../context/UserContext";
@@ -41,9 +40,6 @@ const MainStack = () => {
                 <Stack.Screen name="AccountSettingsScreen" component={AccountSettingsScreen} options={{ title: "Account Settings/Info" }} />
                 <Stack.Screen name="AboutSettingsScreen" component={AboutSettingsScreen} options={{ title: "About" }} />
             </Stack.Group>
-
-            <Stack.Screen name="EventVerificationScreen" component={EventVerification} options={{ title: "Event Verification" }} />
-
         </Stack.Navigator>
     );
 };
