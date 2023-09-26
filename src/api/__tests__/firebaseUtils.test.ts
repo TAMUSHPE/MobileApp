35eb7f4ae6b257673528b4c92f50e0d6b4f290b6
LucientZ <koblenzpoisiden@gmail.com>
2023-09-26 16:11:18 -0500

    Setup jest config and stubbed out first test

---
+++ src/api/__tests__/firebaseUtils.test.ts
@@ -0,0 +1,7 @@
+import { db, auth, storage } from "../../config/firebaseConfig";
+import * as firebaseUtils from "../firebaseUtils";
+
+
+test("Ensure current user data is initialized correctly", () => {
+    expect(true);
+});
