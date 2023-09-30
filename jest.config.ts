b8a36461830976591061a9ab5be4754e0a442903
LucientZ <koblenzpoisiden@gmail.com>
2023-09-30 15:45:40 -0500

    expo-jest now works with firebase

--- jest.config.ts
+++ jest.config.ts
@@ -2,17 +2,11 @@ import type { Config } from "jest";
 
 const config: Config = {
     preset: "jest-expo",
-    transform: {
-        "^.+\\.js$": "babel-jest",
-        "^.+\\.ts$": "ts-jest"
-    },
     transformIgnorePatterns: [
-        "/node_modules/(?!((jest-)?react-native|@firebase/.*|firebase/.*|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native-async-storage/async-storage|node-fetch)",
+        "/node_modules/(?!((jest-)?react-native|@firebase/.*|firebase/.*|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native-async-storage/async-storage|whatwg-fetch)",
     ],
     setupFiles: ["./src/__mocks__/index.ts"],
-    globals: {
-        "fetch": global.fetch,
-    }
+    testEnvironment: 'node',
 };
 
 export default config;
