e43dfc40e29cda76dc1bc62689b19b07ba3901f7
LucientZ <koblenzpoisiden@gmail.com>
2023-09-30 13:56:20 -0500

    Updated firebase and set up jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,17 @@ import type { Config } from "jest";
 
 const config: Config = {
     preset: "jest-expo",
+    transform: {
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.ts$": "ts-jest"
+    },
     transformIgnorePatterns: [
-        "/node_modules/(?!((jest-)?react-native|@firebase/.*|firebase/.*|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+        "/node_modules/(?!((jest-)?react-native|@firebase/.*|firebase/.*|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native-async-storage/async-storage|node-fetch)",
     ],
-    coverageDirectory: "../coverage",
+    setupFiles: ["./src/__mocks__/index.ts"],
+    globals: {
+        "fetch": global.fetch,
+    }
 };
 
 export default config;
