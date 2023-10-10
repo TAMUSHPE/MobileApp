964a329640611d69300de72d5809934777b87c54
jason <jasonisazn@tamu.edu>
2023-10-10 18:45:38 -0500

    remove memmber of the month

--- src/components/HighLightSliderItem.tsx
+++ src/components/HighLightSliderItem.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Image } from 'react-native'
 import React from 'react'
 import { Slide } from './slides'
 
@@ -11,31 +11,14 @@ import { Slide } from './slides'
  * @returns The rendered slider item component.
  */
 const HighLightSliderItem: React.FC<{ item: Slide }> = ({ item }) => {
-    const isMember: boolean = item.type === "member";
-
     return (
         <View className='w-screen'>
-            {!isMember ? (
-                <View className="pt-5 pb-4 ml-7">
-                    <Image
-                        className="h-48 w-[92%] rounded-2xl"
-                        source={item.image}
-                    />
-                </View>
-            ) : (
-                <View className="flex flex-row pt-6 w-screen ml-7">
-                    <View className='w-[50%] mr-4'>
-                        <Image
-                            className="h-48 w-full rounded-2xl"
-                            source={item.image}
-                        />
-                    </View>
-                    <View className='w-36 mt-2'>
-                        <Text className='text-white text-center mb-1 text-xl'>{item.title}</Text>
-                        <Text className='text-white'> {item.description}</Text>
-                    </View>
-                </View>
-            )}
+            <View className="pt-5 pb-4 ml-7">
+                <Image
+                    className="h-48 w-[92%] rounded-2xl"
+                    source={item.image}
+                />
+            </View>
         </View>
     )
 }
