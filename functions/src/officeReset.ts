60585b6d5359485b11becaaf088a63b3d9d29306
jason <jasonisazn@tamu.edu>
2023-10-15 03:02:40 -0500

    fix function, added button to reset office hour (#84)

--- functions/src/officeReset.ts
+++ functions/src/officeReset.ts
@@ -2,12 +2,13 @@
 import * as functions from 'firebase-functions';
 import { db } from "./firebaseConfig"
 
-export const resetOffice = functions.pubsub.schedule('0 5 * * *').timeZone('America/Chicago').onRun(async (context) => {
+
+const resetOffice = async () => {
     const setOfficer = db.doc(`office-hours/officer-count`);
-    const officersRef = db.doc('office-hours/officers-status/officers').collection('officers');
+    const officersRef = db.collection('office-hours/officers-status/officers');
+
     try{
         await setOfficer.update({'zachary-office': 0});
-        // fetch all officers
         officersRef.get().then(async (snapshot) => {
             snapshot.forEach(doc => {
                 doc.ref.update({
@@ -18,28 +19,17 @@ export const resetOffice = functions.pubsub.schedule('0 5 * * *').timeZone('Amer
         return { success: true };
     }
     catch (error) {
-    console.error('Update failure:', error);
-    throw new functions.https.HttpsError('internal', 'Could not reset officer count');
+        console.error('Update failure:', error);
+        throw new functions.https.HttpsError('internal', 'Could not reset officer count');
     }
+
+}
+
+export const resetOfficeScheduler = functions.pubsub.schedule('0 19 * * *') .onRun(async (context) => {
+    resetOffice()
 });
 
-export const testReset = functions.https.onCall(async (context) => {
-    const setOfficer = db.doc(`office-hours/officer-count`);
-    const officersRef = db.doc('office-hours/officers-status/officers').collection('officers');
-    try{
-        await setOfficer.update({'zachary-office': 0});
-        // fetch all officers
-        officersRef.get().then(async (snapshot) => {
-            snapshot.forEach(doc => {
-                doc.ref.update({
-                    signIn: false,
-                });
-            });
-        });
-        return { success: true };
-    }
-    catch (error) {
-    console.error('Update failure:', error);
-    throw new functions.https.HttpsError('internal', 'Could not reset officer count');
-    }
+export const resetOfficeOnCall = functions.https.onCall(async (data, context) => {
+    resetOffice()
+
 });
