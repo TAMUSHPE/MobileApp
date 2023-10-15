27f71183466250a1cf3a57b9286f0b06931a6728
Alvaro Camacho <alvaro.j.camacho.h@gmail.com>
2023-10-15 13:20:04 -0500

    Finished ProgressBar
    Fixed the issue where any update to the ProgressBar where any update would cause the animation to reset.

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Animated, Easing, StyleSheet } from 'react-native';
 
 interface ProgressBarProps {progress: number;} //this is how you pass in percentage. To be used whenever needed
 
 const ProgressBar: React.FC<ProgressBarProps> = ({progress}) => 
 {
-    const progressValue = new Animated.Value(0); //creates the animation
+    progress = progress <= 1 ? progress: 1;
+    progress = progress < 0 ? 0 : progress;
 
-    Animated.timing(progressValue, 
-    {
-        toValue: progress,
-        duration: 1000, //Adjust the duration as needed. pretty sure this is milliseconds but I have not been able to test
-        easing: Easing.linear, //easing means smooth animation
-        useNativeDriver: false, // If set to true, border radius for rounded corners does not work
-    }).start();
+    const [progressValue] = useState(new Animated.Value(0)); //creates the animation
+
+    useEffect(() => { //useEffect makes it so the animation doesn't reset every time
+        Animated.timing(progressValue, 
+            {
+                toValue: progress,
+                duration: 1000, //Adjust the duration as needed. 
+                easing: Easing.linear, //easing means smooth animation
+                useNativeDriver: false, // If set to true, border radius for rounded corners does not work
+            }).start();
+    }, [progress]);
 
     const width = progressValue.interpolate({
-        inputRange: [0, 1], //what is taken in
+        inputRange: [0, 1], //what is taken in (we have accounted for negative values or values greater than 1)
         outputRange: ['0%', '100%'], //what is displayed (on the bar itself)
     });
 
     return (
         <View style={styles.progressBarContainer}>
 
-        <Animated.View style = {[styles.progressBar, {width}]}/> 
+            <Animated.View style = {[styles.progressBar, {width}]}/> 
 
         </View>
     );
@@ -35,21 +40,15 @@ const styles = StyleSheet.create
     progressBarContainer: {
         width: '80%',
         height: 10,
-        backgroundColor: '#cccccc',
+        backgroundColor: '#cccccc', //feel free to change
         borderRadius: 5,
     },
     progressBar: {
         height: '100%',
-        backgroundColor: 'blue', //haven't been able to see this color yet, will set manually when tested for the first time
+        backgroundColor: 'blue', //feel free to change
         borderRadius: 5,
     },
 });
 
 export default ProgressBar;
 
-/**
- * The ProgressBar is yet to be implemented anywhere and I am yet to test any of it. 
- * I also have no way of currently passing in any value to ensure that it works.
- * Once I am able to see what it looks like, I would like to make the necessary changes
- * myself. 
- */
