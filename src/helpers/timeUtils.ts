343eb6f75738c55ee6531394407ee895e9e7cea8
LucientZ <koblenzpoisiden@gmail.com>
2023-11-16 14:35:17 -0600

    Added index.ts to helpers directory

---
+++ src/helpers/timeUtils.ts
@@ -0,0 +1,9 @@
+/**
+ * Set of constants which can be used to do math when it comes to 
+ */
+export abstract class MillisecondTimes {
+    static readonly DAY = 86400000;
+    static readonly HOUR = 3600000;
+    static readonly MINUTE = 60000;
+    static readonly SECOND = 1000;
+}
