5097ebe2b873daefd1ae596f75e402ab0a541e62
jason <jasonisazn@tamu.edu>
2023-11-16 15:44:09 -0600

    enhance styling for login screens

--- src/screens/LoginStudent.tsx
+++ src/screens/LoginStudent.tsx
@@ -1,20 +1,22 @@
-import { View, Text, TextInput, KeyboardAvoidingView, Image, ActivityIndicator } from "react-native";
+import { View, Text, Image, ActivityIndicator, TouchableHighlight, TouchableOpacity } from "react-native";
 import React, { useEffect, useState, useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Google from "expo-auth-session/providers/google";
 import { auth } from "../config/firebaseConfig";
-import { signInWithEmailAndPassword, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
+import { signInWithCredential, GoogleAuthProvider } from "firebase/auth";
 import { initializeCurrentUserData } from "../api/firebaseUtils";
 import { UserContext } from "../context/UserContext";
 import InteractButton from "../components/InteractButton";
 import { AuthStackParams } from "../types/Navigation";
 import { Images } from "../../assets";
 import { validateTamuEmail } from "../helpers/validation";
+import { Octicons } from '@expo/vector-icons';
 
 const LoginStudent = ({ route, navigation }: NativeStackScreenProps<AuthStackParams>) => {
     const [loading, setLoading] = useState<boolean>(false);
+
     const userContext = useContext(UserContext);
     const { userInfo, setUserInfo } = userContext!;
 
@@ -61,7 +63,7 @@ const LoginStudent = ({ route, navigation }: NativeStackScreenProps<AuthStackPar
             const credential = GoogleAuthProvider.credential(id_token);
             signInWithCredential(auth, credential)
                 .then((credential) => {
-                    if(!validateTamuEmail(credential.user.email)){
+                    if (!validateTamuEmail(credential.user.email)) {
                         auth.signOut();
                         alert("Sign in with your Tamu email");
                         return;
@@ -79,31 +81,56 @@ const LoginStudent = ({ route, navigation }: NativeStackScreenProps<AuthStackPar
     }
 
     return (
-        <SafeAreaView className="flex-1 items-center justify-between bg-dark-navy">
-            <View className="flex-col items-center my-8">
+        <SafeAreaView className="flex-1 bg-dark-navy h-screen">
+            <View className='pl-6 mt-2'>
+                <TouchableOpacity
+                    className="pr-4" onPress={() => navigation.navigate("LoginScreen")}
+                    activeOpacity={1}
+                >
+                    <Octicons name="chevron-left" size={30} color="white" />
+                </TouchableOpacity>
+            </View>
+
+            <View className="flex-col items-center my-8 pb-15">
                 <Image
                     className="flex-row h-20 w-20 mb-3"
                     source={Images.SHPE_LOGO}
                 />
-                <Text className="text-white text-center text-3xl">Welcome to SHPE</Text>
             </View>
-            <View className="flex-col w-4/5">
-                <View className="flex-col mt-2">
-                    <InteractButton
-                        onPress={() => googleSignIn()}
-                        label="Sign In with Google"
-                        buttonClassName="bg-white mt-2 rounded-xl"
-                        textClassName="text-[#3b3b3b] font-bold"
-                        iconSource={{ uri: "https://developers.google.com/static/identity/images/g-logo.png" }}
-                    />
-                    {loading && (
-                        <ActivityIndicator className="mt-4" size={"large"} />
-                    )}
-                </View>
+            <View className="items-center pb-40">
+                <View className="flex-col w-4/5">
+                    <Text className="text-white font-bold text-3xl">Student Login</Text>
+                    <Text className="text-pale-orange text-lg">Sign in using tamu.edu email</Text>
 
+                    <View className="flex-col mt-2">
+                        <InteractButton
+                            onPress={() => googleSignIn()}
+                            label="Sign In with Google"
+                            buttonClassName="bg-white mt-5 rounded-3xl py-1"
+                            textClassName="text-[#3b3b3b] font-bold text-xl"
+                            iconSource={{ uri: "https://developers.google.com/static/identity/images/g-logo.png" }}
+                        />
+                        {loading && (
+                            <ActivityIndicator className="mt-4" size={"large"} />
+                        )}
+                    </View>
+                    <View className="items-center">
+                    </View>
+                </View>
             </View>
-            <View className="my-5 w-11/12">
-                <Text className="text-right text-pale-orange mt-2">{"Society of Hispanic\nProfessional\nEngineers"}</Text>
+
+            <View className="flex-1 justify-end items-center">
+                <View className="flex-row items-center">
+                    <Text className="text-slate-400 text-lg">Don't have a student account?</Text>
+                    <TouchableOpacity
+                        className="items-center"
+                        onPress={() => navigation.navigate("LoginGuest")}
+                        activeOpacity={1}
+                    >
+                        <Text className="text-pale-orange text-lg ml-1">Guest Login</Text>
+                    </TouchableOpacity>
+
+                </View>
             </View>
         </SafeAreaView>
     );
