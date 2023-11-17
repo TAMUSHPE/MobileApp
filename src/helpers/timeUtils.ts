99d99ee834980943eb83ecd3d38224298584d151
LucientZ <koblenzpoisiden@gmail.com>
2023-11-17 08:34:59 -0600

    Modified each event to instead have a copyFromObject() method instead of taking it from the constructor

--- src/helpers/timeUtils.ts
+++ src/helpers/timeUtils.ts
@@ -1,9 +1,18 @@
 /**
- * Set of constants which can be used to do math when it comes to 
+ * Set of constants which can be used to do math with milliseconds
  */
 export abstract class MillisecondTimes {
     static readonly DAY = 86400000;
     static readonly HOUR = 3600000;
     static readonly MINUTE = 60000;
     static readonly SECOND = 1000;
+}
+
+/**
+ * Returns the number of milliseconds elapsed since midnight, January 1, 1970 Universal Coordinated Time (UTC) rounded up to the next hour. 
+ * @returns unix timestamp of next hour
+ */
+export const getNextHourMillis = (): number => {
+    const currentTime = Date.now();
+    return currentTime + MillisecondTimes.HOUR - (currentTime % MillisecondTimes.HOUR);
 }
