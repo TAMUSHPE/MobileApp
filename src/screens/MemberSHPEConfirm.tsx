361922911fa6635d5c42d0bbd1a162e7f87d37ef
jason <jasonisazn@tamu.edu>
2023-11-27 13:35:23 -0600

    display members to verify & fix typo (#157)

--- src/screens/MemberSHPEConfirm.tsx
+++ src/screens/MemberSHPEConfirm.tsx
@@ -1,12 +1,42 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { PublicUserInfo } from '../types/User'
+import { getMembersToVerify } from '../api/firebaseUtils'
+import MembersList from '../components/MembersList'
 
-const memberSHPEConfirm = () => {
+const MemberSHPEConfirm = () => {
+    // get user info from memberSHPe collection
+    // put into list and use membersList component to display
+    // when user clicks on member, display member confirmation approve/deny modal
+    // allow admin to view chapter proof and national proof
+    // if approve, mark flags as true in user's document and remove from memberSHPE collection
+    // if deny, remove from memberSHPE collection
+    const [members, setMembers] = useState<PublicUserInfo[]>([]);
+
+    useEffect(() => {
+        const fetchMembers = async () => {
+            try {
+                const fetchedMembers = await getMembersToVerify();
+                setMembers(fetchedMembers);
+            } catch (error) {
+                console.error('Error fetching members:', error);
+            }
+        };
+
+        fetchMembers();
+    }, []);
+
     return (
         <View>
-            <Text>memberSHPEConfirm</Text>
+            <MembersList
+                handleCardPress={(uid) => {
+                    console.log(uid)
+                }}
+                officersList={[]}
+                membersList={members}
+            />
         </View>
     )
 }
 
-export default memberSHPEConfirm
+export default MemberSHPEConfirm
