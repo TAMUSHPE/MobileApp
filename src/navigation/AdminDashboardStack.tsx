a4588eeb8307ac58ae0c9c2708e5f02d1a02a3a8
jason <jasonisazn@tamu.edu>
2023-11-27 13:35:23 -0600

    display members to verify & fix typo (#157)

--- src/navigation/AdminDashboardStack.tsx
+++ src/navigation/AdminDashboardStack.tsx
@@ -10,7 +10,7 @@ import ResumeDownloader from "../screens/ResumeDownloader";
 import ResetOfficeHours from "../screens/ResetOfficeHours";
 import { Button } from "react-native";
 import { NavigationProp, useNavigation } from "@react-navigation/core";
-import memberSHPEConfirm from "../screens/memberSHPEConfirm";
+import MemberSHPEConfirm from "../screens/MemberSHPEConfirm";
 
 const AdminDashboardStack = () => {
     const Stack = createNativeStackNavigator<AdminDashboardParams>();
@@ -52,7 +52,7 @@ const AdminDashboardStack = () => {
                 <Stack.Screen name="ResumeDownloader" component={ResumeDownloader} />
                 <Stack.Screen name="ResetOfficeHours" component={ResetOfficeHours} />
                 <Stack.Screen name="RestrictionsEditor" component={RestrictionsEditor} />
-                <Stack.Screen name="MemberSHPEConfirm" component={memberSHPEConfirm} />
+                <Stack.Screen name="MemberSHPEConfirm" component={MemberSHPEConfirm} />
             </Stack.Group>
         </Stack.Navigator >
     );
