83897648fc317c6103cad8a1f5f89cfeff7081e8
jason <jasonisazn@tamu.edu>
2023-11-27 12:59:20 -0600

    init member SHPE confirmation for admin side (#157)

---
+++ src/screens/MemberSHPEConfirm.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const memberSHPEConfirm = () => {
+    return (
+        <View>
+            <Text>memberSHPEConfirm</Text>
+        </View>
+    )
+}
+
+export default memberSHPEConfirm
