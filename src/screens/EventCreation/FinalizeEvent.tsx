0c1a0cf4d81590db8f08e378ce93754e889b21b0
LucientZ <koblenzpoisiden@gmail.com>
2023-11-30 15:34:55 -0600

    Added event creation screens to EventStack

---
+++ src/screens/EventCreation/FinalizeEvent.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const FinalizeEvent = () => {
+    return (
+        <View>
+            <Text>FinalizeEvent</Text>
+        </View>
+    );
+};
+
+export default FinalizeEvent;
---
+++ src/screens/EventCreation/SetSpecificEventDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const SetSpecificEventDetails = () => {
+    return (
+        <View>
+            <Text>SetSpecificEventDetails</Text>
+        </View>
+    );
+};
+
+export default SetSpecificEventDetails;
