cd848002487f269f9db0146a5337be2c6bd05e4c
jason <jasonisazn@tamu.edu>
2023-12-06 00:21:57 -0600

    rearrange resume cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,6 @@ export { sendNotificationOfficeHours, sendNotificationMemberSHPE } from "./pushN
 export { updateRanksScheduled, updateRanksOnCall } from "./pointSheet";
 export { updateCommitteeCount, updateCommitteesCount, incrementCommitteesCount, countCommitteeMembersOnCall, countCommitteeMembers } from "./committees";
 export { resetOfficeScheduler, resetOfficeOnCall } from "./officeReset";
-export { zipResume } from "./resumeZip";
+export { zipResume } from "./resume";
 export { updateUserRole } from "./roles";
 export { checkUsernameUniqueness } from "./checkUsername";
