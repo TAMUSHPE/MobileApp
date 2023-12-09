43d68cc7a3a9167ad7ad073d9145a7a69a471437
jason <jasonisazn@tamu.edu>
2023-12-09 01:34:52 -0600

    fix rate limiting and improve black/watch list functions

--- src/api/firebaseUtils.ts
+++ src/api/firebaseUtils.ts
@@ -354,46 +354,31 @@ export const deleteCommittee = async (firebaseDocName: string): Promise<void> =>
 };
 
 export const getWatchlist = async () => {
-    return getDoc(doc(db, `restrictions/watchlist`))
-        .then((res) => {
-            const responseData = res.data()
-            return responseData?.UIDs;
-        })
-        .catch(err => {
-            console.error(err);
-            return undefined;
-        });
-}
+    const docRef = doc(db, "restrictions/watchlist");
+    const docSnap = await getDoc(docRef);
+    return docSnap.exists() ? docSnap.data().UIDs : [];
+};
 
 export const getBlacklist = async () => {
-    return getDoc(doc(db, `restrictions/blacklist`))
-        .then((res) => {
-            const responseData = res.data()
-            return responseData?.UIDs;
-        })
-        .catch(err => {
-            console.error(err);
-            return undefined;
-        });
-}
+    const docRef = doc(db, "restrictions/blacklist");
+    const docSnap = await getDoc(docRef);
+    return docSnap.exists() ? docSnap.data().UIDs : [];
+};
 
-export const setWatchlist = async (watchlist: string[]) => {
-    try {
-        await setDoc(doc(db, `restrictions/watchlist`), {UIDs: watchlist}, { merge: true });
-        return true;
-    } catch (err) {
-        console.error(err);
-        return false;
+
+export const addToWatchlist = async (uid: string) => {
+    const currentWatchlist = await getWatchlist() || [];
+    if (!currentWatchlist.includes(uid)) {
+        const updatedWatchlist = [...currentWatchlist, uid];
+        await setDoc(doc(db, "restrictions/watchlist"), { UIDs: updatedWatchlist }, { merge: true });
     }
 };
 
-export const setBlacklist = async (blacklist: string[]) => {
-    try {
-        await setDoc(doc(db, `restrictions/blacklist`), {UIDs: blacklist}, { merge: true });
-        return true;
-    } catch (err) {
-        console.error(err);
-        return false;
+export const addToBlacklist = async (uid: string) => {
+    const currentBlacklist = await getBlacklist() || [];
+    if (!currentBlacklist.includes(uid)) {
+        const updatedBlacklist = [...currentBlacklist, uid];
+        await setDoc(doc(db, "restrictions/blacklist"), { UIDs: updatedBlacklist }, { merge: true });
     }
 };
 
