28eabda8a1f4fd2905102a43efcea87ef61191ee
jason <jasonisazn@tamu.edu>
2023-12-17 22:44:31 -0600

    added button component for resources and small detailing for office hours

--- src/screens/Resources.tsx
+++ src/screens/Resources.tsx
@@ -1,6 +1,6 @@
-import { View, Image, ScrollView, Text, TouchableOpacity, Linking } from 'react-native';
+import { View, Image, ScrollView, Text, TouchableOpacity, Linking, ImageSourcePropType } from 'react-native';
 import React from 'react';
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { ResourcesStackParams } from '../types/Navigation';
 import { Images } from '../../assets';
@@ -31,8 +31,46 @@ const Resources = ({ navigation }: { navigation: NativeStackNavigationProp<Resou
             });
     };
 
+
+    const SocialMediaButton = ({ url, imageSource, bgColor = "" }: {
+        url: string,
+        imageSource: ImageSourcePropType,
+        bgColor?: string
+    }) => (
+        <TouchableOpacity
+            className='flex-col items-center'
+            onPress={() => handleLinkPress(url)}
+        >
+            <View className={`h-14 w-14 rounded-full items-center justify-center ${bgColor}`}>
+                <Image source={imageSource} className={bgColor != "" ? "w-11 h-11" : "w-14 h-14"} />
+            </View>
+        </TouchableOpacity>
+    );
+
+    const ResourceButton = ({ title, navigation, navigateTo, IconComponent }: {
+        title: string;
+        navigation: NativeStackNavigationProp<any, any>;
+        navigateTo: string;
+        IconComponent: React.ElementType;
+    }) => (
+        <TouchableOpacity
+            className='flex-row bg-pale-blue w-[85%] h-24 rounded-3xl mb-8'
+            onPress={() => navigation.navigate(navigateTo)}
+        >
+            <View className='w-[73%] flex-row items-end px-6 py-3'>
+                <Text className='text-white font-bold text-2xl'>{title}</Text>
+            </View>
+            <View className='flex-1 justify-center items-center rounded-r-3xl'
+                style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}
+            >
+                <IconComponent width={50} height={50} />
+            </View>
+        </TouchableOpacity>
+    );
+
     return (
         <View style={{ paddingTop: insets.top }} className='flex-1 bg-white'>
+            {/* Header */}
             <View className='flex-row px-5 pb-4'>
                 <View className='flex-1 justify-center items-start'>
                     <Image
@@ -41,102 +79,57 @@ const Resources = ({ navigation }: { navigation: NativeStackNavigationProp<Resou
                     />
                 </View>
             </View>
+
             <ScrollView >
                 {/* Links */}
                 <View className='flex-row mx-2 mt-4 justify-evenly'>
-                    <TouchableOpacity
-                        className='flex-col items-center'
-                        onPress={() => handleLinkPress('https://www.geneva.com/')}
-                    >
-                        <View className='h-14 w-14 rounded-full bg-[#A1CEFE] items-center justify-center'>
-                            <Image source={Images.GENEVA} className='h-10 w-10 rounded-full' />
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        className='flex-col items-center'
-                        onPress={() => handleLinkPress('https://www.instagram.com/tamushpe/')}
-                    >
-                        <View className='h-14 w-14 rounded-full items-center justify-center'>
-                            <Image source={Images.INSTAGRAM} className='h-14 w-14' />
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        className='flex-col items-center'
-                        onPress={() => handleLinkPress('https://www.flickr.com/photos/143848472@N03/albums/')}
-                    >
-
-                        <View className='h-14 w-14 rounded-full items-center justify-center'>
-                            <Image source={Images.FLICKER} className='h-14 w-14' />
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        className='flex-col items-center'
-                        onPress={() => handleLinkPress('https://www.tamushpe.org/')}
-                    >
-                        <View className='h-14 w-14 rounded-full bg-maroon items-center justify-center'>
-                            <Image source={Images.TAMU_WHITE} className='h-12 w-12 rounded-full' />
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        className='flex-col items-center'
-                        onPress={() => handleLinkPress('https://shpe.org/')}
-                    >
-                        <View className='h-14 w-14 rounded-full bg-dark-navy items-center justify-center'>
-                            <Image source={Images.SHPE_LOGO} className='h-12 w-12 rounded-full' />
-                        </View>
-                    </TouchableOpacity>
+                    <SocialMediaButton
+                        url="https://www.geneva.com/"
+                        imageSource={Images.GENEVA}
+                        bgColor="bg-[#A1CEFE]"
+                    />
+                    <SocialMediaButton
+                        url="https://www.instagram.com/tamushpe/"
+                        imageSource={Images.INSTAGRAM}
+                    />
+                    <SocialMediaButton
+                        url="https://www.flickr.com/photos/143848472@N03/albums/"
+                        imageSource={Images.FLICKER}
+                    />
+                    <SocialMediaButton
+                        url="https://www.tamushpe.org/"
+                        imageSource={Images.TAMU_WHITE}
+                        bgColor='bg-maroon'
+                    />
+                    <SocialMediaButton
+                        url="https://shpe.org/"
+                        imageSource={Images.SHPE_LOGO}
+                        bgColor='bg-dark-navy'
+                    />
                 </View>
 
                 {/* Resources */}
-                <View className='flex-col mt-12 items-center space-y-8'>
-                    <TouchableOpacity
-                        className='flex-row bg-pale-blue w-[85%] h-24 rounded-3xl'
-                        onPress={() => navigation.navigate("PointsLeaderboard")}
-                    >
-                        <View className='w-[73%] flex-row items-end px-6 py-3'>
-                            <Text className='text-white font-bold text-2xl'>Points Leaderboard</Text>
-                        </View>
-                        <View className='flex-1 justify-center items-center rounded-r-3xl'
-                            style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}
-                        >
-                            <LeaderBoardIcon width={50} height={50} />
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        className='flex-row bg-pale-blue w-[85%] h-24 rounded-3xl'
-                        onPress={() => navigation.navigate("TestBank")}
-                    >
-                        <View className='w-[73%] flex-row items-end px-6 py-3'>
-                            <Text className='text-white font-bold text-2xl'>Test Bank</Text>
-                        </View>
-                        <View className='flex-1 justify-center items-center rounded-r-3xl'
-                            style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}
-                        >
-                            <ExamIcon width={50} height={50} />
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        className='flex-row bg-pale-blue w-[85%] h-24 rounded-3xl'
-                        onPress={() => navigation.navigate("ResumeBank")}
-                    >
-                        <View className='w-[73%] flex-row items-end px-6 py-3'>
-                            <Text className='text-white font-bold text-2xl'>Resume Bank</Text>
-                        </View>
-                        <View className='flex-1 justify-center items-center rounded-r-3xl'
-                            style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}
-                        >
-                            <ResumeIcon width={50} height={50} />
-                        </View>
-                    </TouchableOpacity>
+                <View className='flex-col mt-12 items-center'>
+                    <ResourceButton
+                        title="Points Leaderboard"
+                        navigation={navigation}
+                        navigateTo="PointsLeaderboard"
+                        IconComponent={LeaderBoardIcon}
+                    />
+                    <ResourceButton
+                        title="Test Bank"
+                        navigation={navigation}
+                        navigateTo="TestBank"
+                        IconComponent={ExamIcon}
+                    />
+                    <ResourceButton
+                        title="Resume Bank"
+                        navigation={navigation}
+                        navigateTo="ResumeBank"
+                        IconComponent={ResumeIcon}
+                    />
                 </View>
 
-                {/* Office Hours */}
                 <OfficeHours />
 
                 <View className='mb-12' />
