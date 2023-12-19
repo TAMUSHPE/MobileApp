4678da67ed06426960a064166ca67c8ac43c5e8b
jason <jasonisazn@tamu.edu>
2023-12-18 23:14:33 -0600

    add committee logo selector

--- src/types/Committees.ts
+++ src/types/Committees.ts
@@ -1,6 +1,23 @@
 import { ImageSourcePropType } from "react-native";
 import { PublicUserInfo } from "./User";
 
+import SHPEtinas from '../../assets/shpetinas.svg';
+import TechnicalAffairs from '../../assets/technical_affairs_logo.svg';
+import DefaultIcon from '../../assets/generic_course_icon.svg';
+export const committeeLogos = {
+    shpetinas: SHPEtinas,
+    technicalAffairs: TechnicalAffairs,
+    default: DefaultIcon,
+};
+
+
+export const getLogoComponent = (logoName: keyof typeof committeeLogos = 'default') => {
+    return committeeLogos[logoName];
+};
+
+export type CommitteeLogosName = keyof typeof committeeLogos;
+
+
 export type Committee = {
     name?: string;
     firebaseDocName?: string;
@@ -12,4 +29,6 @@ export type Committee = {
     memberCount?: number;
     memberApplicationLink?: string;
     leadApplicationLink?: string;
+    logo?: keyof typeof committeeLogos;
 }
+
