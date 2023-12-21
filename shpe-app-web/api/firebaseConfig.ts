01deda25bb9cc4e8c1cf879b364bebe55a9e86ab
Axel Fontaine <22axfontaine@gmail.com>
2023-12-21 14:53:07 -0600

    Rough Firebase Auth Implementation

---
+++ shpe-app-web/api/firebaseConfig.ts
@@ -0,0 +1,34 @@
+import { initializeApp, FirebaseApp, getApp, getApps } from 'firebase/app';
+import { getAuth, Auth, initializeAuth, signInWithPopup, signOut, GoogleAuthProvider, User} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getFunctions } from 'firebase/functions';
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+    apiKey: "***REMOVED***",
+    authDomain: "tamushpemobileapp.firebaseapp.com",
+    projectId: "tamushpemobileapp",
+    storageBucket: "tamushpemobileapp.appspot.com",
+    messagingSenderId: "600060629240",
+    appId: "1:600060629240:web:1e97e43973746bcc266b0d"
+};
+
+let app: FirebaseApp;
+// let auth: Auth;
+
+if (getApps().length === 0) {
+    app = initializeApp(firebaseConfig);
+    // auth = initializeAuth(app);
+}
+else {
+    app = getApp();
+    // auth = getAuth(app);
+}
+
+const auth = getAuth(app)
+const db = getFirestore(app);
+const storage = getStorage(app);
+const functions = getFunctions(app);
+
+export { db, auth, storage, functions};
