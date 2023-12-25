7bac03e15bf890b69a0a178da26a99d44fe3fa24
jason <jasonisazn@tamu.edu>
2023-12-25 03:01:25 -0600

    ui change to admin dashboard

--- src/screens/admin/AdminDashboard.tsx
+++ src/screens/admin/AdminDashboard.tsx
@@ -1,9 +1,10 @@
-import { Text, TouchableOpacity } from 'react-native'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { httpsCallable, getFunctions } from 'firebase/functions';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AdminDashboardParams } from '../../types/Navigation';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Octicons } from '@expo/vector-icons';
 
 const AdminDashboard = ({ navigation }: NativeStackScreenProps<AdminDashboardParams>) => {
     const updateRanks = async () => {
@@ -18,76 +19,116 @@ const AdminDashboard = ({ navigation }: NativeStackScreenProps<AdminDashboardPar
         committeeCountCheckOnCall()
     }
 
-
     return (
-        <SafeAreaView className='items-center justify-center space-y-4' edges={['top']}>
-            <TouchableOpacity
-                onPress={() => updateRanks()}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Update Points and Rank for all Users</Text>
-            </TouchableOpacity>
+        <View className='flex-1'>
+            <SafeAreaView edges={['top']} >
+                <View className='flex-row items-center mx-5 mt-1'>
+                    <View className='absolute w-full justify-center items-center'>
+                        <Text className="text-2xl font-semibold" >Admin Dashboard</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => navigation.goBack()} className='py-2'>
+                        <Octicons name="chevron-left" size={30} color={"black"} />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
 
-            <TouchableOpacity
-                onPress={() => getCommitteesCount()}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Load Committee Membership Count</Text>
-            </TouchableOpacity>
+            <ScrollView>
+                <View className='py-4 px-2 w-full mx-5'>
+                    <Text className='text-2xl font-semibold mb-3'>Create</Text>
+                    <View className='flex-row flex-wrap'>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('CommitteeCreator')}
+                            className='bg-pale-blue rounded-md py-4 px-2 items-center justify-center'
+                        >
+                            <Text className='text-white text-xl'>Committee</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
+                <View className='py-4 px-2 mx-5'>
+                    <Text className='text-2xl font-semibold mb-3'>Verification</Text>
+                    <View className='flex-row flex-wrap'>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('MemberSHPEConfirm')}
+                            className='bg-red-orange rounded-md py-4 px-2 items-center justify-center mr-4'
+                        >
+                            <Text className='text-white text-xl'>MemberSHPE</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('ResumeConfirm')}
+                            className='bg-red-orange rounded-md py-4 px-2 items-center justify-center'
+                        >
+                            <Text className='text-white text-xl'>Resume Bank</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-            <TouchableOpacity
-                onPress={() => navigation.navigate('CommitteeCreator')}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Create committee</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('MemberOfTheMonthEditor')}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Update Member of the Month</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('FeaturedSlideEditor')}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Home Featured Slider</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('ResumeDownloader')}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Resume Downloader</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('ResetOfficeHours')}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Test Reset Office Hour</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('RestrictionsEditor')}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Update Restrictions</Text>
-            </TouchableOpacity>
+                <View className='py-4 px-2 mx-5'>
+                    <Text className='text-2xl font-semibold mb-3'>Other Tools</Text>
+                    <View className='flex-row flex-wrap'>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('MemberOfTheMonthEditor')}
+                            className='bg-dark-navy rounded-md py-4 px-2 items-center justify-center mr-4'
+                        >
+                            <Text className='text-white text-xl'>MOTM</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('FeaturedSlideEditor')}
+                            className='bg-dark-navy rounded-md py-4 px-2 items-center justify-center'
+                        >
+                            <Text className='text-white text-xl'>Featured Images</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('ResumeDownloader')}
+                            className='bg-dark-navy rounded-md py-4 px-2 items-center justify-center mt-3'
+                        >
+                            <Text className='text-white text-xl'>Resumes Download</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-            <TouchableOpacity
-                onPress={() => navigation.navigate('MemberSHPEConfirm')}
-                className='bg-blue-500 rounded-md p-2'
-            >
-                <Text>Member SHPE Verifications</Text>
-            </TouchableOpacity>
+                <View className='py-4 px-2 mx-5'>
+                    <Text className='text-2xl font-semibold mb-3'>Admin</Text>
+                    <View className='flex-row flex-wrap '>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('RestrictionsEditor')}
+                            className='bg-pale-orange rounded-md py-4 px-2 items-center justify-center'
+                        >
+                            <Text className='text-white text-xl'>Restriction</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-            <TouchableOpacity
-                onPress={() => navigation.navigate('ResumeConfirm')}
-                className='bg-blue-500 rounded-md p-2'>
-                <Text>Resume Verifications</Text>
-            </TouchableOpacity>
+                <View className='py-4 px-2 mx-5'>
+                    <Text className='text-2xl font-semibold mb-3'>Developer Testing</Text>
+                    <View className='flex-row flex-wrap '>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('ResetOfficeHours')}
+                            className='bg-black rounded-md py-4 px-2 items-center justify-center'
+                        >
+                            <Text className='text-white text-xl'>Rest Office</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => updateRanks()}
+                            className='bg-black rounded-md py-4 px-2 items-center justify-center mt-3'
+                        >
+                            <Text className='text-white text-xl'>Update Points and Rank for all Users</Text>
+                        </TouchableOpacity>
 
-        </SafeAreaView>
+                        <TouchableOpacity
+                            onPress={() => getCommitteesCount()}
+                            className='bg-black rounded-md py-4 px-2 items-center justify-center mt-3'
+                        >
+                            <Text className='text-white text-xl'>Load Committee Membership Count</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View className='pb-20' />
+            </ScrollView>
+        </View>
     )
 }
 
 export default AdminDashboard
+
