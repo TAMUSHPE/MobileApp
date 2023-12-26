290f97938cdffff1870493e60874378d82644211
jason <jasonisazn@tamu.edu>
2023-12-25 21:23:59 -0600

    update ui for resume download and fix small detail for resume downloading

--- src/screens/admin/ResumeDownloader.tsx
+++ src/screens/admin/ResumeDownloader.tsx
@@ -1,49 +1,64 @@
-import { Button, ActivityIndicator, View, Text } from 'react-native';
+import { ActivityIndicator, View, Text, TouchableOpacity } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { getFunctions, httpsCallable } from 'firebase/functions';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Octicons } from '@expo/vector-icons';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
 import { db } from '../../config/firebaseConfig';
+import { getFunctions, httpsCallable } from 'firebase/functions';
 import { doc, onSnapshot } from 'firebase/firestore';
-import * as Clipboard from 'expo-clipboard';
 import { handleLinkPress } from '../../helpers/links';
+import { AdminDashboardParams } from '../../types/Navigation';
+
+interface ResumeDownloadInfo {
+    url: string;
+    createdAt: Date;
+    expiresAt: Date;
+}
+const ResumeDownloader = ({ navigation }: NativeStackScreenProps<AdminDashboardParams>) => {
+    const [isGenerated, setIsGenerated] = useState(false);
+    const [resumeDownloadInfo, setResumeDownloadInfo] = useState<ResumeDownloadInfo>();
+    const { url: downloadUrl, createdAt, expiresAt } = resumeDownloadInfo || {};
+
+    const formatDate = (date: Date) => {
+        if (!date) return '';
+        return `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
+    };
+
+    const formatDateForFileName = (date: Date) => {
+        const pad = (num: number) => num.toString().padStart(2, '0');
+        const year = date.getFullYear();
+        const month = pad(date.getMonth() + 1);
+        const day = pad(date.getDate());
+
+        return `${year}-${month}-${day}`;
+    };
 
 
-const ResumeDownloader = () => {
-    const [loading, setLoading] = useState(false);
-    const [downloadUrl, setDownloadUrl] = useState('');
-    const [createdAt, setCreatedAt] = useState('');
-    const [expiresAt, setExpiresAt] = useState('');
-    const [showCopyNotification, setShowCopyNotification] = useState(false);
-
-
-    const zipResumes = async () => {
-        setLoading(true);
-        const functions = getFunctions();
-        const zipResumesFunction = httpsCallable(functions, 'zipResume');
-        try {
-            await zipResumesFunction();
-        } catch (error) {
-            console.error('Error:', error);
-        }
+    const isExpired = (expiresAt: Date) => {
+        const now = new Date();
+        return now > expiresAt;
     };
 
     useEffect(() => {
-        const statusRef = doc(db, 'resumes', 'status');
-        const dataRef = doc(db, 'resumes', 'data');
+        const statusRef = doc(db, 'resumes/status');
+        const dataRef = doc(db, 'resumes/data');
 
         const unsubscribeStatus = onSnapshot(statusRef, (docSnapshot) => {
             if (docSnapshot.exists()) {
                 const statusData = docSnapshot.data();
-                setLoading(!statusData.isGenerated);
+                setIsGenerated(statusData.isGenerated);
             }
         });
 
         const unsubscribeData = onSnapshot(dataRef, (docSnapshot) => {
             if (docSnapshot.exists()) {
-                const data = docSnapshot.data();
-                setDownloadUrl(data.url);
-                setCreatedAt(data.createdAt.toDate().toString());
-                setExpiresAt(data.expiresAt.toDate().toString());
+                setResumeDownloadInfo({
+                    url: docSnapshot.data().url,
+                    createdAt: docSnapshot.data().createdAt.toDate(),
+                    expiresAt: docSnapshot.data().expiresAt.toDate(),
+                });
             }
         });
 
@@ -53,45 +68,90 @@ const ResumeDownloader = () => {
         };
     }, []);
 
-    const copyToClipboard = async () => {
-        await Clipboard.setStringAsync(downloadUrl);
-        setShowCopyNotification(true);
+    const zipResumes = async () => {
+        setIsGenerated(false);
+        const functions = getFunctions();
+        const zipResumesFunction = httpsCallable(functions, 'zipResume');
+        try {
+            await zipResumesFunction();
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
-    useEffect(() => {
-        let timerId: NodeJS.Timeout;
-        if (showCopyNotification) {
-            timerId = setTimeout(() => {
-                setShowCopyNotification(false); // Hide the notification after 2 seconds
-            }, 2000);
-        }
-        return () => clearTimeout(timerId);
-    }, [showCopyNotification]);
+    const shareDownloadUrl = async () => {
+        try {
+            const formattedDate = formatDateForFileName(createdAt!);
+            const fileName = `TAMU_SHPE_Resumes_${formattedDate}.zip`;
+            const localUri = await FileSystem.downloadAsync(downloadUrl!, FileSystem.documentDirectory + fileName);
 
+            await Sharing.shareAsync(localUri.uri);
+        } catch (error) {
+            console.error('Error sharing the file:', error);
+        }
+    };
 
     return (
         <SafeAreaView>
-            {!loading && (
-                <View>
-                    <Button title="Generate Resumes" onPress={zipResumes} />
-                    <Button title="Open Download Link" onPress={() => handleLinkPress(downloadUrl)} disabled={!downloadUrl} />
-                    <Button title="Copy Download Link" onPress={copyToClipboard} disabled={!downloadUrl} />
-                    <Text>Created At: {createdAt}</Text>
-                    <Text>Expires At: {expiresAt}</Text>
+            <View className='flex-row items-center mx-5 mt-1'>
+                <View className='absolute w-full justify-center items-center'>
+                    <Text className="text-2xl font-semibold" >Resume Download</Text>
                 </View>
-            )}
-            {loading && (
-                <View>
-                    <ActivityIndicator size="large" />
-                    <Text> New Resume folder is being generated</Text>
-                </View>
-            )}
-            {showCopyNotification && (
-                <Text className="absolute bottom-0 bg-green-300 items-center justify-center">
-                    Link copied to clipboard!
-                </Text>
-            )}
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Octicons name="chevron-left" size={30} color="black" />
+                </TouchableOpacity>
+            </View>
 
+            <View className='space-y-4 mx-6 mt-10'>
+                <Text className='text-lg font-semibold'>Instructions</Text>
+                <Text>This is a resume bundle downloader that downloads all resume that a user has uploaded </Text>
+
+                <Text>Click the button below to generate a URL link. This URL link will contain a direct download for a zip file. This URL Link will be valid for only <Text className='font-bold'>1 hour</Text></Text>
+
+                <Text>The generation of this URL link will vary and depends on the amount of resume being generated. This can take up to 20 minutes. <Text className='font-bold'>You may leave the app and come back</Text>.</Text>
+
+                <Text>When the link is generated confirm the creation time. You may either open the link or share it</Text>
+
+                <Text style={{ color: "red" }}>NEVER SHARED THIS LINK WITH ANYONE OTHER THAN YOUR OWN DEVICES OR AUTHORIZED PERSONNEL</Text>
+
+                <View className='w-full items-center'>
+                    <TouchableOpacity
+                        className='bg-pale-blue items-center justify-center w-36 py-2 rounded-md'
+                        onPress={zipResumes}>
+                        <Text className='text-white font-semibold text-xl'>Generate</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+
+            {/* Show Download/Copy If Expiration Date did not pass & IsGenerated == true */}
+
+            {!isExpired(expiresAt!) && isGenerated && (
+                <View className='mx-7 mt-10'>
+                    <Text className='text-md font-semibold'>Date Created</Text>
+                    <Text className='text-md font-semibold'>{formatDate(createdAt!)}</Text>
+                    <View className='flex-row w-full items-center justify-around mt-4'>
+                        <TouchableOpacity
+                            className='bg-pale-blue items-center justify-center w-36 py-2 rounded-md'
+                            onPress={() => handleLinkPress(downloadUrl!)}
+                        >
+                            <Text className='text-white font-semibold text-xl'>Open Link</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            className='bg-pale-blue items-center justify-center w-36 py-2 rounded-md'
+                            onPress={() => shareDownloadUrl()}>
+                            <Text className='text-white font-semibold text-xl'>Share Link</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )
+
+            }
+            {!isGenerated && (
+                <View className='flex justify-center items-center mt-10'>
+                    <ActivityIndicator size="large" />
+                </View>
+            )}
         </SafeAreaView >
     );
 }
