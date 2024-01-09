c196cc3b3380c47826497faa9ac52721efcaee64
Jason <jasonisazn@tamu.edu>
2024-01-09 15:35:48 -0600

    hide api key

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
+    plugins: [
+      "nativewind/babel",
+      "react-native-reanimated/plugin",
+      [
+        "inline-dotenv",
+        {
+          unsafe: true,
+        },
+      ],
+    ],
   };
 };
