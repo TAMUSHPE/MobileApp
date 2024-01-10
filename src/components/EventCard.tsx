aeee6e3d8ce63b6058b0bc321597dcdc307502c9
LucientZ <koblenzpoisiden@gmail.com>
2024-01-09 22:36:39 -0600

    Modified appearance of events list and event card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -2,10 +2,10 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import { EventProps, UpdateEventScreenRouteProp } from '../types/Navigation'
 import { monthNames } from '../types/Events'
+import { Images } from '../../assets'
+import { Octicons } from '@expo/vector-icons';
 
 const EventCard: React.FC<EventProps> = ({ event, navigation }) => {
-    const startDateAsDate = event?.startTime?.toDate() ?? null;
-
     const formatDate = (date: Date) => {
         const day = date.getDate();
         const month = monthNames[date.getMonth()];
@@ -13,22 +13,27 @@ const EventCard: React.FC<EventProps> = ({ event, navigation }) => {
 
         return `${month} ${day} ${year}`;
     }
+
     return (
         <TouchableOpacity onPress={() => navigation.navigate("EventInfo", { eventId: event?.id! })}>
-            <View className='flex-row'>
-                <View className='justify-center ml-2 mr-4 py-2 '>
-                    <Image
-                        source={require("../../assets/github-logo.png")}
-                        className='h-12 w-12 rounded-full'
-                        resizeMode='contain'
-                    />
-                </View>
-                <View>
-                    {startDateAsDate &&
-                        <Text className='text-red-500 text-lg font-bold'>{formatDate(startDateAsDate)}</Text>
+            <View className='h-20 flex flex-row rounded-md my-2 overflow-hidden'>
+                <View className='h-full w-[20%] justify-center items-center bg-slate-300'>
+                    {event?.coverImageURI &&
+                        <Image
+                            source={{ uri: event.coverImageURI }}
+                            className='min-h-full min-w-full'
+                        />
                     }
-                    <Text className='text-xl font-bold'>{event?.name}</Text>
-                    <Text className='text-lg text-grey'>{event?.locationName}</Text>
+                    {!event?.coverImageURI &&
+                        <Octicons name='repo' size={50} />
+                    }
+                </View>
+                <View className='h-full flex flex-col flex-1 pl-4 bg-slate-200'>
+                    {event?.startTime &&
+                        <Text className='text-red-500 text-lg font-bold flex-1'>{formatDate(event?.startTime?.toDate() ?? null)}</Text>
+                    }
+                    <Text className='text-xl font-bold flex-1'>{event?.name}</Text>
+                    <Text className='text-lg text-grey flex-1'>{event?.locationName}</Text>
                 </View>
             </View>
         </TouchableOpacity>
