858992ea9945e9d4942525e4c84e257911f3cfd7
LucientZ <koblenzpoisiden@gmail.com>
2024-01-15 02:19:30 -0600

    Added point calculation functions

--- functions/src/pointSheet.ts
+++ functions/src/pointSheet.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import { db } from "./firebaseConfig"
 import { RankChange } from "./types";
+import { AggregateField } from 'firebase-admin/firestore';
 
 /** Determines rank change based on current and new ranks. */
 const getRankChange = (userData: any, newRank: number): RankChange => {
@@ -64,4 +65,66 @@ export const updateRanksOnCall = functions.https.onCall(async (data, context) =>
         console.error("Error in updatePointStats:", error);
         throw new functions.https.HttpsError('internal', 'Internal Server Error.');
     }
+});
+
+/**
+ * Sums all user points from their event log collection and returns this value
+ * @param uid Universal identifier for user
+ * @returns Total points user has earned from events
+ */
+const calculateUserPoints = async (uid: string): Promise<number> => {
+    const collectionRef = db.collection(`users/${uid}/event-logs`);
+    const sumAggregateQuery = collectionRef.aggregate({
+        totalPoints: AggregateField.sum('points'),
+    });
+
+    return sumAggregateQuery.get()
+        .then((snapshot) => {
+            return snapshot.data().totalPoints;
+        })
+        .catch((err: any) => {
+            console.error(`Issue updating points for user with UID ${uid}:`, err);
+            return 0;
+        });
+};
+
+/**
+ * Sums all user points from their event log collection from the past month and returns this value
+ * @param uid Universal identifier for user
+ * @returns Total points user has earned from events over the past month
+ */
+const calculateUserPointsThisMonth = async (uid: string): Promise<number> => {
+    // TODO: Currently Stubbed
+    return 0;
+}
+
+/** Callable https function which updates the point values of a given user users */
+export const updateUserPoints = functions.https.onCall(async (data, context) => {
+    if (!context.auth) {
+        throw new functions.https.HttpsError("unauthenticated", "Function cannot be called without authentication.");
+    } else if (typeof data.uid !== "string") {
+        throw new functions.https.HttpsError("invalid-argument", "Invalid data types passed into function");
+    }
+
+    const token = context.auth.token;
+    if (token.admin !== true && token.officer !== true && token.developer !== true) {
+        throw new functions.https.HttpsError("permission-denied", `Invalid credentials`);
+    }
+
+    const userDocRef = db.doc(`users/${data.uid}`);
+    return userDocRef.set({
+        points: await calculateUserPoints(data.uid),
+        pointsThisMonth: await calculateUserPointsThisMonth(data.uid),
+    }, { merge: true })
+        .then(() => {
+            return { success: true }
+        })
+        .catch((err) => {
+            throw new functions.https.HttpsError("aborted", `Issue occured while attempting to update user document: ${err}`);
+        });
+});
+
+/** Callable https function which updates the point values of all users */
+export const updateAllUserPoints = functions.https.onCall(async (data, context) => {
+    // TODO: Currently Stubbed
 });
