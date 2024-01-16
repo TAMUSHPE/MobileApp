e04b87ee3745a4f920e9c78cf1511d28f613b94c
LucientZ <koblenzpoisiden@gmail.com>
2024-01-15 22:33:38 -0600

    Added creator information to event

--- src/types/Events.ts
+++ src/types/Events.ts
@@ -1,5 +1,6 @@
 import { GeoPoint, Timestamp } from 'firebase/firestore';
 import { MillisecondTimes, getNextHourMillis } from '../helpers';
+import { auth } from '../config/firebaseConfig';
 
 /**
  * Type used specifically for Workshop events to differentiate the type of workshop
@@ -47,6 +48,11 @@ export abstract class SHPEEvent {
     /** Specifies which committee this event is associated with */
     public committee?: string | null;
 
+    public creatorUID?: string | null;
+    public creatorDisplayName?: string | null;
+    public creatorProfilePictureURI?: string | null;
+
+
     /**
      * Instantiates all fields that exist on all SHPE Events
      */
@@ -61,6 +67,9 @@ export abstract class SHPEEvent {
         this.committee = null;
         this.geolocation = null;
         this.geofencingRadius = null;
+        this.creatorUID = auth.currentUser?.uid ?? null;
+        this.creatorDisplayName = auth.currentUser?.displayName ?? null;
+        this.creatorProfilePictureURI = auth.currentUser?.photoURL ?? null;
     }
 
     /**
