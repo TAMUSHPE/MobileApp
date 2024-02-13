10308e5dd3a83867404602569ca47f422a7e8f4f
Mark Gonzalez <markgonzalez@Marks-MacBook-Pro-67.local>
2024-02-13 11:24:57 -0600

    Navbar, pages for screens

---
+++ shpe-app-web/app/committees/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const Committees = () => {
+  return (
+     <div className= 'bg-white'>
+
+      <Navbar />
+
+      </div>
+  );
+}
+
+export default Committees;
