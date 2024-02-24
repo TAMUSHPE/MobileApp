5e6cf6133db4a84174830ad01c971aaeb22be71f
Alvaro Camacho <alvaro.j.camacho.h@gmail.com>
2024-02-23 18:42:00 -0600

    Changed location of user shirt size data
    Changed the location of the user shirt size data from their private data to a new collection called "shirtSizes". Next step is to give admins access to this data.

--- src/api/firebaseUtils.ts
+++ src/api/firebaseUtils.ts
@@ -805,13 +805,6 @@ export const setUserRoles = async (uid: string, roles: Roles): Promise<HttpsCall
         });
 };
 
-export const setUserShirtSize = async (shirtSize: string): Promise<void> => {
-    await setDoc(doc(db, `memberSHPE/${auth.currentUser?.uid}`), {
-        shirtSize: shirtSize
-    }, { merge: true });
-};
-
-
 export const getMembersExcludeOfficers = async (): Promise<PublicUserInfo[]> => {
     try {
         const userRef = collection(db, 'users');
