3ca00c803942914ea0ef816b90936e0b0d009c9a
Axel Fontaine <22axfontaine@gmail.com>
2024-02-23 18:35:11 -0600

    root git ignore

--- shpe-app-web/api/firebaseConfig.ts
+++ shpe-app-web/api/firebaseConfig.ts
@@ -1,5 +1,5 @@
 import { initializeApp, FirebaseApp, getApp, getApps } from 'firebase/app';
-import { getAuth, Auth, initializeAuth, signInWithPopup, signOut, GoogleAuthProvider, User } from "firebase/auth";
+import { getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from 'firebase/functions';
 import { getStorage } from "firebase/storage";
