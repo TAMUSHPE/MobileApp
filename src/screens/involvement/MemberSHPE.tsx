6193433c86c7ddd616553b7511ffb620b941c2b0
Alvaro Camacho <alvaro.j.camacho.h@gmail.com>
2024-02-23 18:02:08 -0600

    Fixed MemberSHPE Approval Bug
    Fixed the MemberSHPE approval bug where a user would appear to be approved while not having uploaded both of their required documents. Thanks Robbie.

--- src/screens/involvement/MemberSHPE.tsx
+++ src/screens/involvement/MemberSHPE.tsx
@@ -82,7 +82,7 @@ const MemberSHPE = () => {
     const onNationalUploadSuccess = async (URL: string) => {
         const today = new Date();
         const expirationDate = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000); // 1 year from now
-        await setDoc(doc(db, `shirtOrder/${auth.currentUser?.uid}`), {
+        await setDoc(doc(db, `memberSHPE/${auth.currentUser?.uid}`), {
             nationalUploadDate: Timestamp.fromDate(today),
             nationalExpiration: Timestamp.fromDate(expirationDate),
             nationalURL: URL
@@ -104,6 +104,12 @@ const MemberSHPE = () => {
             chapterExpiration: Timestamp.fromDate(expirationDate),
             chapterURL: URL
         }, { merge: true });
+
+        //await setDoc(doc(db, `shirtOrder/${auth.currentUser?.uid}`), {
+            //shirtUploadDate: Timestamp.fromDate(today),
+            //shirtSize: //shirt size here
+        //}, { merge: true });
+
         setLoading(false);
         //setShowShirtModal(true)
     };
