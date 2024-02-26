26f89cc1a3c43737066dcb20b4fa5ab243be8390
Jason <jasonisazn@tamu.edu>
2024-02-26 12:57:10 -0600

    merge web

---
+++ shpe-app-web/app/points/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const Points = () => {
+  return (
+     <div className= 'bg-white'>
+
+      <Navbar />
+
+      </div>
+  );
+}
+
+export default Points;
