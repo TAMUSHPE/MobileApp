d43a9266f43414b60bdc5cf0e10790ee9e04027a
Jason <jasonisazn@tamu.edu>
2024-02-28 13:42:24 -0600

    fix update user crashing app

--- App.tsx
+++ App.tsx
@@ -1,30 +1,9 @@
 import 'react-native-gesture-handler';
 import React, { useEffect } from "react";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { UserProvider } from './src/context/UserContext';
 import RootNavigator from './src/navigation';
-import { auth } from './src/config/firebaseConfig';
-import { getUser } from './src/api/firebaseUtils';
-import { eventEmitter } from './src/context/eventEmitter';
-
 export default function App() {
-    useEffect(() => {
-        const subscriber = auth.onAuthStateChanged(async (user) => {
-            if (user) {
-                try {
-                    const userFromFirebase = await getUser(user.uid);
-                    await AsyncStorage.setItem("@user", JSON.stringify(userFromFirebase));
-                    eventEmitter.emit("userUpdated");
-                } catch (error) {
-                    console.error("Error updating user:", error);
-                }
-            }
-        });
-
-        return () => subscriber();
-    }, []);
-
     return (
         <SafeAreaProvider>
             <UserProvider>
