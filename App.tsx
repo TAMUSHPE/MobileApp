86c40f5fe40edd48b2ba63b32de2ff3d17e361db
Jason <jasonisazn@tamu.edu>
2024-02-28 16:16:34 -0600

    add notification back

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { Alert } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { UserProvider } from './src/context/UserContext';
 import RootNavigator from './src/navigation';
+import * as Notifications from 'expo-notifications';
+
 export default function App() {
+    const notificationListener = useRef<Subscription | null>();
+    const responseListener = useRef<Subscription | null>();
+    useEffect(() => {
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            Alert.alert(notification.request.content.body as string);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            if (notificationListener.current) {
+                notificationListener.current.remove();
+            }
+            if (responseListener.current) {
+                responseListener.current.remove();
+            }
+        };
+    }, []);
     return (
         <SafeAreaProvider>
             <UserProvider>
@@ -12,3 +35,5 @@ export default function App() {
         </SafeAreaProvider>
     );
 };
+
+type Subscription = { remove: () => void };
