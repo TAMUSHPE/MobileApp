4d5996e11496e1f86bf1a6788a6f6febfef07b93
Jason <jasonisazn@tamu.edu>
2024-03-26 15:13:39 -0500

    fix formating, text color, and animation name

--- src/screens/events/EventVerification.tsx
+++ src/screens/events/EventVerification.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Alert } from 'react-native'
+import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react';
 import { useRoute } from '@react-navigation/core';
 import { EventVerificationProps, EventVerificationScreenRouteProp } from '../../types/Navigation'
@@ -8,13 +8,14 @@ import LottieView from "lottie-react-native";
 import { EventLogStatus } from '../../types/Events';
 import { ActivityIndicator } from "react-native";
 import * as Haptics from 'expo-haptics';
+import { StatusBar } from 'expo-status-bar';
 
 const EventVerification = ({ navigation }: EventVerificationProps) => {
     const route = useRoute<EventVerificationScreenRouteProp>();
     const { id, mode } = route.params;
     const [logStatus, setLogStatus] = useState<EventLogStatus>();
-    console.log(logStatus);
     const [loading, setLoading] = useState<boolean>(true);
+
     useEffect(() => {
         switch (mode) {
             case "sign-in":
@@ -46,14 +47,15 @@ const EventVerification = ({ navigation }: EventVerificationProps) => {
 
     return (
         <SafeAreaView className={`w-screen h-screen   bg-dark-navy ${(logStatus === EventLogStatus.SUCCESS) && "bg-green-500"}`}>
-            {
-                loading &&
+            <StatusBar style="light" />
+
+            {loading &&
                 <View className='w-full h-full items-center justify-center'>
                     <ActivityIndicator className='mt-4' size={"large"} />
                 </View>
             }
-            {
-                (logStatus === EventLogStatus.SUCCESS) &&
+
+            {(logStatus === EventLogStatus.SUCCESS) &&
                 <View className='w-screen h-[70%] items-center justify-center'>
                     <View className=' w-screen h-40 '>
                         <LottieView
@@ -66,22 +68,25 @@ const EventVerification = ({ navigation }: EventVerificationProps) => {
                     <Text className='text-lg font-semibold'>You've successfully signed in</Text>
                 </View>
             }
-            {
-                (logStatus === EventLogStatus.ALREADY_LOGGED) &&
+
+            {(logStatus === EventLogStatus.ALREADY_LOGGED) &&
                 <View className='w-screen h-[70%] items-center justify-center'>
                     <View className=' w-screen h-40 '>
                         <LottieView
-                            source={require("../../../assets/check_animation.json")}
+                            source={require("../../../assets/red_x_animation.json")}
                             autoPlay
                             loop={false}
-                            onAnimationFinish={() => { redirectToPage(); Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success) }}
+                            onAnimationFinish={() => {
+                                redirectToPage();
+                                Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
+                            }}
                         />
                     </View>
-                    <Text className='text-lg font-semibold'>You have already logged into this event.</Text>
+                    <Text className='text-lg font-semibold text-white'>You have already logged into this event.</Text>
                 </View>
             }
-            {
-                (logStatus === EventLogStatus.ERROR) &&
+
+            {(logStatus === EventLogStatus.ERROR) &&
                 <View className='w-screen h-[70%] items-center justify-center'>
                     <View className=' w-screen h-40 '>
                         <LottieView
@@ -91,49 +96,56 @@ const EventVerification = ({ navigation }: EventVerificationProps) => {
                             onAnimationFinish={() => { redirectToPage(); Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error) }}
                         />
                     </View>
-                    <Text className='text-lg font-semibold'>An internal error occurred while attempting. Please try again.</Text>
+                    <Text className='text-lg font-semibold text-white'>An internal error occurred while attempting. Please try again.</Text>
                 </View>
             }
-            {
-                (logStatus === EventLogStatus.EVENT_NOT_STARTED) &&
+
+            {(logStatus === EventLogStatus.EVENT_NOT_STARTED) &&
                 <View className='w-screen h-[70%] items-center justify-center'>
                     <View className=' w-screen h-40 '>
                         <LottieView
                             source={require("../../../assets/red_x_animation.json")}
                             autoPlay
                             loop={false}
-                            onAnimationFinish={() => { redirectToPage(); Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error) }}
+                            onAnimationFinish={() => {
+                                redirectToPage();
+                                Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
+                            }}
                         />
                     </View>
-                    <Text className='text-lg font-semibold'>This event has not started yet.</Text>
+                    <Text className='text-lg font-semibold text-white'>This event has not started yet.</Text>
                 </View>
             }
-            {
-                (logStatus === EventLogStatus.EVENT_OVER) &&
+            {(logStatus === EventLogStatus.EVENT_OVER) &&
                 <View className='w-screen h-[70%] items-center justify-center'>
                     <View className=' w-screen h-40 '>
                         <LottieView
                             source={require("../../../assets/red_x_animation.json")}
                             autoPlay
                             loop={false}
-                            onAnimationFinish={() => { redirectToPage(); Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error) }}
+                            onAnimationFinish={() => {
+                                redirectToPage();
+                                Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
+                            }}
                         />
                     </View>
-                    <Text className='text-lg font-semibold'>This event has already ended.</Text>
+                    <Text className='text-lg font-semibold text-white'>This event has already ended.</Text>
                 </View>
             }
-            {
-                (logStatus === EventLogStatus.EVENT_NOT_FOUND) &&
+            {(logStatus === EventLogStatus.EVENT_NOT_FOUND) &&
                 <View className='w-screen h-[70%] items-center justify-center'>
                     <View className=' w-screen h-40 '>
                         <LottieView
                             source={require("../../../assets/red_x_animation.json")}
                             autoPlay
                             loop={false}
-                            onAnimationFinish={() => { redirectToPage(); Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error) }}
+                            onAnimationFinish={() => {
+                                redirectToPage();
+                                Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
+                            }}
                         />
                     </View>
-                    <Text className='text-lg font-semibold'>This event could not be found. Please try either </Text>
+                    <Text className='text-lg font-semibold text-white'>This event could not be found. Please try either </Text>
                 </View>
             }
         </SafeAreaView>
