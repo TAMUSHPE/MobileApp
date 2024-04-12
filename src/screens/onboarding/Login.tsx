7630a0892a53d762a5cef35dcb668ccc6b0398b5
Alvaro Camacho <alvaro.j.camacho.h@gmail.com>
2024-04-12 18:27:31 -0500

    Login page updated
    Login page updated to match the Figma

--- src/screens/onboarding/Login.tsx
+++ src/screens/onboarding/Login.tsx
@@ -13,40 +13,49 @@ const LoginScreen = ({ navigation }: NativeStackScreenProps<AuthStackParams>) =>
     return (
         <SafeAreaView className="flex-1 justify-between bg-dark-navy items-center">
             <View className="flex-col items-center my-8">
-                <View className="flex-row">
+            <Text className="text-white text-center text-4xl font-bold" style={{marginTop : 60}}>Welcome to SHPE</Text>
+                <View className="flex-row" style={{marginTop:60}}>
                     <Image
-                        className="flex-row h-20 w-20 mb-3"
+                        className="flex-row h-32 w-32 mb-3"
                         source={Images.SHPE_LOGO}
                     />
                 </View>
-                <Text className="text-white text-center text-4xl font-bold">Welcome to SHPE</Text>
             </View>
 
-            <View className="flex-col w-[80%] pb-28">
+            <View className="flex-col w-[80%]" style={{marginTop: -90}}>
                 <Text className="text-white text-2xl font-bold">Login as a</Text>
                 <View className="flex-col mt-2">
                     <InteractButton
                         onPress={() => navigation.navigate('LoginStudent')}
                         label="Student"
-                        buttonClassName="justify-center items-center bg-continue-dark mt-5 rounded-xl py-1"
+                        buttonClassName="justify-center items-center bg-continue-dark mt-5 rounded-xl py-0"
                         textClassName="text-white font-bold text-xl"
                         underlayColor="#A22E2B"
                     />
+
+                    <View className="flex-col  justify-center" style={{ alignItems: 'center', marginTop : 10, marginBottom : -15}}>
+                        <Text className="text-white text-center font-bold" style={{ fontSize : 15}}>
+                            {"or"}
+                        </Text>
+                    </View>
+
                     <InteractButton
                         onPress={() => navigation.navigate('LoginGuest')}
                         label="Guest"
-                        buttonClassName="justify-center items-center bg-continue-dark mt-7 rounded-xl py-1"
+                        buttonClassName="justify-center items-center bg-continue-dark mt-7 rounded-xl py-0"
                         textClassName="text-white font-bold text-xl"
                         underlayColor="#A22E2B"
                     />
                 </View>
             </View>
-            <View className="my-5 w-11/12 flex-row justify-between">
-                <Text className="text-left text-white font-bold">
-                    {"Texas A&M University,\nCollege Station,\nTX"}
+            <View className="flex-row justify-center" style={{ marginBottom : -100}}>
+                <Text className="text-center font-bold" style={{ color: '#FD652F' }}>
+                    {"Society of Hispanic Professional Engineers"}
                 </Text>
-                <Text className="text-right text-pale-orange font-bold">
-                    {"Society of Hispanic\nProfessional\nEngineers"}
+            </View>
+            <View className="flex-row justify-center">
+                <Text className="text-center font-bold" style={{ color: '#FFFFFF' }}>
+                    {"Texas A&M University.\nCollege Station, TX"}
                 </Text>
             </View>
         </SafeAreaView>
