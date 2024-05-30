b522682254fb936b184f7281e15d7c8d3d9dfe36
Jason <jasonisazn@tamu.edu>
2024-05-30 01:37:46 -0500

    add event filter (#363)

--- src/screens/events/Events.tsx
+++ src/screens/events/Events.tsx
@@ -7,7 +7,7 @@ import { Octicons, FontAwesome } from '@expo/vector-icons';
 import { UserContext } from '../../context/UserContext';
 import { getUpcomingEvents, getPastEvents } from '../../api/firebaseUtils';
 import { EventsStackParams } from '../../types/Navigation';
-import { SHPEEvent } from '../../types/Events';
+import { EventType, SHPEEvent } from '../../types/Events';
 import CalendarICON from '../../../assets/calandar_pale_blue.svg'
 import EventsList from '../../components/EventsList';
 
@@ -21,13 +21,28 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
     const [isLoading, setIsLoading] = useState(true);
     const [pastEventModalVisible, setPastEventModalVisible] = useState(false);
     const [initialFetch, setInitialFetch] = useState(false);
-    const [initialPastFetch, setInitialPastFetch] = useState(false);
-
-
+    // const [initialPastFetch, setInitialPastFetch] = useState(false);
+    const [selectedFilter, setSelectedFilter] = useState<EventType | null>(null);
 
     const hasPrivileges = (userInfo?.publicInfo?.roles?.admin?.valueOf() || userInfo?.publicInfo?.roles?.officer?.valueOf() || userInfo?.publicInfo?.roles?.developer?.valueOf());
 
     const insets = useSafeAreaInsets();
+
+    const handleFilterSelect = (filter: EventType) => {
+        if (selectedFilter === filter) {
+            setSelectedFilter(null);
+        } else {
+            setSelectedFilter(filter);
+        }
+    };
+
+    const filteredEvents = (events: SHPEEvent[]) => {
+        if (!selectedFilter) {
+            // By default, hide committee meetings because they may increase clutter
+            return events.filter(event => event.eventType !== EventType.COMMITTEE_MEETING);
+        }
+        return events.filter(event => event.eventType === selectedFilter);
+    };
 
     useFocusEffect(
         useCallback(() => {
@@ -36,7 +51,7 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
                     setIsLoading(true);
 
                     const upcomingEventsData = await getUpcomingEvents();
-                    const pastEventsData = await getPastEvents(5);
+                    const pastEventsData = await getPastEvents(8);
 
                     // Filter to separate current and upcoming events
                     const currentTime = new Date();
@@ -77,9 +92,8 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
         }, [hasPrivileges, initialFetch])
     );
 
-
     return (
-        <SafeAreaView edges={["top"]}>
+        <SafeAreaView edges={["top"]} className='h-full'>
             <ScrollView>
                 <View className='flex-row mt-4'>
                     <View className='w-full justify-center items-center'>
@@ -97,16 +111,28 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
                             <Text className='font-bold text-pale-blue text-lg ml-2'>QRCode Scan</Text>
                         </TouchableOpacity>
                     )}
-
-                    {hasPrivileges &&
-                        <TouchableOpacity
-                            className='flex-1 flex-row items-center justify-center border border-pale-blue rounded-sm py-2 mx-2'
-                            onPress={() => navigation.navigate("CreateEvent")}>
-                            <CalendarICON width={24} height={24} />
-                            <Text className='font-bold text-pale-blue text-lg ml-2'>Create Event</Text>
-                        </TouchableOpacity>
-                    }
                 </View>
+
+                {/* Filters */}
+                <ScrollView horizontal={true} className='mt-3' showsHorizontalScrollIndicator={false}>
+                    <View className='flex-row'>
+                        {Object.values(EventType).map((type) => (
+                            <TouchableOpacity
+                                key={type}
+                                className={`flex-row items-center justify-center border rounded-md py-2 px-4 mx-2 mb-2 ${selectedFilter === type ? 'bg-pale-blue' : 'border-pale-blue'}`}
+                                onPress={() => handleFilterSelect(type)}
+                            >
+                                <Text className={`font-bold ${selectedFilter === type ? 'text-white' : 'text-pale-blue'}`}>{type}</Text>
+                            </TouchableOpacity>
+                        ))}
+                        <TouchableOpacity
+                            className='flex-row items-center justify-center border border-red-400 rounded-md py-2 px-4 mx-2 mb-2'
+                            onPress={() => setSelectedFilter(null)}
+                        >
+                            <Text className='font-bold text-red-400'>Reset Filter</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
 
                 {isLoading &&
                     <View className='h-64 justify-center items-center'>
@@ -114,47 +140,52 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
                     </View>
                 }
 
-                {(upcomingEvents.length == 0 && pastEvents.length == 0 && !isLoading) &&
-                    <View className='h-64 w-full justify-center items-center'>
-                        <Text>No Events</Text>
+                {/* Event Listings */}
+                {!isLoading && (
+                    <View className='mx-5'>
+                        {filteredEvents(currentEvents).length === 0 && filteredEvents(upcomingEvents).length === 0 && filteredEvents(pastEvents).length === 0 ? (
+                            <View className='h-32 w-full justify-center items-center'>
+                                <Text className='text-lg font-bold'>No Events</Text>
+                            </View>
+                        ) : (
+                            <View>
+                                {filteredEvents(currentEvents).length !== 0 && (
+                                    <View>
+                                        <Text className='text-xl mb-2 mt-8 font-bold'>On-Going Events</Text>
+                                        <EventsList
+                                            events={filteredEvents(currentEvents)}
+                                            navigation={navigation}
+                                        />
+                                    </View>
+                                )}
+
+                                {filteredEvents(upcomingEvents).length !== 0 && (
+                                    <View>
+                                        <Text className='text-xl mb-2 mt-8 font-bold'>Upcoming Events</Text>
+                                        <EventsList
+                                            events={filteredEvents(upcomingEvents)}
+                                            navigation={navigation}
+                                        />
+                                    </View>
+                                )}
+
+                                {filteredEvents(pastEvents).length !== 0 && (
+                                    <View>
+                                        <Text className='text-xl mb-2 mt-8 font-bold '>Past Events</Text>
+                                        <EventsList
+                                            events={filteredEvents(pastEvents)}
+                                            navigation={navigation}
+                                        />
+                                    </View>
+                                )}
+                            </View>
+                        )}
                     </View>
-                }
-                <View className='mx-5 mt-4'>
-                    {(!isLoading && currentEvents.length != 0) &&
-                        <>
-                            <Text className='text-xl mb-2 mt-8 font-bold'>On-Going Events</Text>
-                            <EventsList
-                                events={currentEvents}
-                                navigation={navigation}
-                            />
-                        </>
-                    }
+                )}
 
-
-                    {(!isLoading && upcomingEvents.length != 0) &&
-                        <>
-                            <Text className='text-xl mb-2 mt-8 font-bold'>Upcoming Events</Text>
-                            <EventsList
-                                events={upcomingEvents}
-                                navigation={navigation}
-                            />
-                        </>
-                    }
-
-                    {(!isLoading && pastEvents.length != 0) &&
-                        <>
-                            <Text className='text-xl mb-2 mt-8 font-bold '>Past Events</Text>
-                            <EventsList
-                                events={pastEvents}
-                                navigation={navigation}
-                            />
-                        </>
-                    }
-                </View>
-
-                {(!isLoading && pastEvents.length >= 5) &&
+                {/* {(!isLoading && pastEvents.length >= 5) &&
                     <TouchableOpacity
-                        className='flex-row items-center justify-center border border-pale-blue rounded-sm py-2 mx-2 my-4'
+                        className='flex-row items-center justify-center border border-pale-blue rounded-sm py-2 mx-2 my-4 w-1/2'
                         onPress={async () => {
                             setPastEventModalVisible(true)
 
@@ -172,10 +203,20 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
                         }}>
                         <Text className='font-bold text-pale-blue text-lg ml-2'>View All Past Events</Text>
                     </TouchableOpacity>
-                }
+                } */}
 
-
+                <View className='pb-20' />
             </ScrollView>
+
+            {hasPrivileges && (
+                <TouchableOpacity
+                    className='absolute bottom-5 right-5 bg-pale-blue rounded-full h-16 w-16 shadow-lg justify-center items-center'
+                    onPress={() => navigation.navigate("CreateEvent")}
+                >
+                    <Octicons name="plus" size={24} color="white" />
+                </TouchableOpacity>
+            )}
+
             <Modal
                 animationType="slide"
                 transparent={true}
@@ -199,8 +240,6 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
                             <Octicons name="x" size={26} color="black" />
                         </TouchableOpacity>
                     </View>
-
-
 
                     <ScrollView>
                         <View className='mx-5 mt-4'>
@@ -227,8 +266,7 @@ const Events = ({ navigation }: NativeStackScreenProps<EventsStackParams>) => {
                     </ScrollView>
                 </View>
             </Modal>
-
-        </SafeAreaView >
+        </SafeAreaView>
     );
 };
 
