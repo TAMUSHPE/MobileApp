e48f77b330478ddf2042774dff232268a42bee48
Jason <jasonisazn@tamu.edu>
2024-06-02 21:48:21 -0500

    remove splash screen, add photo gallery default image

--- src/components/FlickrPhotoGallery.tsx
+++ src/components/FlickrPhotoGallery.tsx
@@ -1,37 +1,23 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { useEffect, useState, useRef, memo } from 'react';
 import { Animated, Image, Dimensions, View, NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
+import { Images } from '../../assets';
 
 const windowWidth = Dimensions.get('window').width;
 
-const flickerApiKey = process.env.FLICKER_API_KEY;
-const flickerUserId = process.env.FLICKER_USER_ID;
-const flickrPhotoSetId = '72177720316068498';
-
-
-const FlickrPhotoItem = memo(({ item }: { item: FlickrPhoto }) => {
-    if (!item) return null;
-
-    const photoUrl = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_w.jpg`;
-    return (
-        <View style={{ width: windowWidth }}>
-            <LinearGradient
-                className='absolute top-0 left-0 bottom-0 right-0'
-                colors={['#ffffff', '#72A9BE']}
-            />
-            <View className="mt-5 pb-4 ml-7">
-                <Image source={{ uri: photoUrl }} className="h-40 w-[92%] rounded-3xl" />
-            </View>
-        </View>
-    );
-});
 
 const FlickrPhotoGallery = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
-    const [photos, setPhotos] = useState<FlickrPhoto[]>([]);
+    const [photos, setPhotos] = useState<(FlickrPhoto | null)[]>([]);
+    const [photosFetched, setPhotosFetched] = useState(false); // Add this flag
     const photoListRef = useRef<Animated.FlatList>(null);
     const scrollX = useRef(new Animated.Value(0)).current;
     const slideInterval = useRef<NodeJS.Timeout>();
+
+    const flickerApiKey = process.env.FLICKER_API_KEY;
+    const flickerUserId = "143848472@N03";
+    const flickrPhotoSetId = '72177720316068498';
+
 
     const shufflePhotos = (array: FlickrPhoto[]) => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -42,16 +28,24 @@ const FlickrPhotoGallery = () => {
     };
 
     const fetchPhotos = async () => {
+
         try {
             const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${flickerApiKey}&user_id=${flickerUserId}&photoset_id=${flickrPhotoSetId}&format=json&nojsoncallback=1`;
 
             const response = await fetch(url);
             const json = await response.json();
 
-            const shuffledPhotos = shufflePhotos(json.photoset.photo);
-            setPhotos([shuffledPhotos[shuffledPhotos.length - 1], ...shuffledPhotos, shuffledPhotos[0]]);
+            if (json && json.photoset && json.photoset.photo.length > 0) {
+                const shuffledPhotos = shufflePhotos(json.photoset.photo);
+                setPhotos([shuffledPhotos[shuffledPhotos.length - 1], ...shuffledPhotos, shuffledPhotos[0]]);
+                setPhotosFetched(true); // Set the flag to true
+            } else {
+                setPhotos([null]); // Add a null item to trigger the default image
+                setPhotosFetched(false); // Ensure flag is false if no photos
+            }
         } catch (error) {
-            console.error(error);
+            setPhotos([null]); // Add a null item to trigger the default image
+            setPhotosFetched(false); // Ensure flag is false if error
         }
     };
 
@@ -67,7 +61,9 @@ const FlickrPhotoGallery = () => {
             setCurrentIndex(prevIndex => {
                 let nextIndex = prevIndex + 1;
                 if (nextIndex >= photos.length) {
-                    photoListRef.current?.scrollToIndex({ animated: false, index: 1 });
+                    if (photos.length > 2) {
+                        photoListRef.current?.scrollToIndex({ animated: false, index: 1 });
+                    }
                     nextIndex = 1;
                 } else {
                     photoListRef.current?.scrollToIndex({ animated: true, index: nextIndex });
@@ -98,7 +94,7 @@ const FlickrPhotoGallery = () => {
     };
 
     useEffect(() => {
-        if (photos.length > 0) {
+        if (photos.length > 2) {
             photoListRef.current?.scrollToIndex({ animated: false, index: 1 });
             resetAndStartAutoSlide();
         }
@@ -111,15 +107,45 @@ const FlickrPhotoGallery = () => {
         index,
     });
 
-    const renderItem = ({ item }: { item: FlickrPhoto }) => <FlickrPhotoItem item={item} />;
+    const FlickrPhotoItem = memo(({ item }: { item: FlickrPhoto | null }) => {
+        const photoUrl = item ? `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_w.jpg` : null;
+        return (
+            <View style={{ width: windowWidth }}>
+                <LinearGradient
+                    className='absolute top-0 left-0 bottom-0 right-0'
+                    colors={['#ffffff', '#72A9BE']}
+                />
+                <View className="mt-5 pb-4 ml-7">
+                    <Image
+                        source={photoUrl ? { uri: photoUrl } : Images.CAROUSEL_1}
+                        className="h-40 w-[92%] rounded-3xl"
+                    />
+                </View>
+            </View>
+        );
+    });
 
-    if (!photos.length) return null;
+    const renderItem = ({ item }: { item: FlickrPhoto | null }) => <FlickrPhotoItem item={item} />;
+
+    if (!photosFetched) {
+        return (
+            <View style={{ width: windowWidth }}>
+                <LinearGradient
+                    className='absolute top-0 left-0 bottom-0 right-0'
+                    colors={['#ffffff', '#72A9BE']}
+                />
+                <View className="mt-5 pb-4 ml-7">
+                    <Image source={Images.CAROUSEL_1} className="h-40 w-[92%] rounded-3xl" />
+                </View>
+            </View>
+        );
+    }
 
     return (
         <Animated.FlatList
             data={photos}
             renderItem={renderItem}
-            keyExtractor={(item, index) => `${item.id} - ${index}`}
+            keyExtractor={(item, index) => `${item ? item.id : 'default'} - ${index}`}
             horizontal
             pagingEnabled
             showsHorizontalScrollIndicator={false}
