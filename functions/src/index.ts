e1edcdf3f548a524451c6066125c48243bf6f162
Jason <jasonisazn@tamu.edu>
2024-08-16 14:28:49 -0500

    add committee count scheduler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 export { notifyUpcomingEvents, sendNotificationOfficeHours, sendNotificationMemberSHPE, sendNotificationCommitteeRequest, sendNotificationResumeConfirm } from "./pushNotification";
 export { updateUserPoints, updateAllUserPoints, scheduledUpdateAllPoints } from "./pointSheet";
-export { updateCommitteeCount } from "./committees";
+export { updateCommitteeCount, scheduleCommitteeCount } from "./committees";
 // export { resetOfficeScheduler } from "./officeReset";
 export { resetOfficeOnCall } from "./officeReset";
 export { zipResume } from "./resume";
