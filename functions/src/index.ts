05c35e6955a4e9a6504317969bbd13f9b571174e
Jason <jasonisazn@tamu.edu>
2024-09-10 13:21:06 -0500

    add cloud function back

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ export { notifyUpcomingEvents, sendNotificationOfficeHours, sendNotificationMemb
 export { updateUserPoints, updateAllUserPoints, scheduledUpdateAllPoints } from "./pointSheet";
 export { updateCommitteeCount, scheduleCommitteeCount } from "./committees";
 // export { resetOfficeScheduler } from "./officeReset";
+export { isUserInBlacklist } from "./restriction";
 export { resetOfficeOnCall } from "./officeReset";
 export { zipResume } from "./resume";
 export { updateUserRole } from "./roles";
