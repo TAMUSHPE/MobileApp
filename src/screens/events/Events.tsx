94b67d25b7b6acd5660e9fe72fe3544a6eaed581
Jason <jasonisazn@tamu.edu>
2024-09-13 19:14:26 -0500

    remove filters

--- src/screens/events/Events.tsx
+++ src/screens/events/Events.tsx
@@ -24,20 +24,16 @@ const Events = ({ navigation }: EventsProps) => {
     const userContext = useContext(UserContext);
     const { userInfo, setUserInfo } = userContext!;
 
-
     const fixDarkMode = userInfo?.private?.privateInfo?.settings?.darkMode;
     const useSystemDefault = userInfo?.private?.privateInfo?.settings?.useSystemDefault;
     const colorScheme = useColorScheme();
     const darkMode = useSystemDefault ? colorScheme === 'dark' : fixDarkMode;
 
-    const filterScrollViewRef = useRef<ScrollView>(null);
-    const committeeScrollViewRef = useRef<ScrollView>(null);
 
     const [isLoading, setIsLoading] = useState(true);
     const [todayEvents, setTodayEvents] = useState<SHPEEvent[]>([]);
     const [upcomingEvents, setUpcomingEvents] = useState<SHPEEvent[]>([]);
     const [pastEvents, setPastEvents] = useState<SHPEEvent[]>([]);
-    const [selectedFilter, setSelectedFilter] = useState<ExtendedEventType | null>(route.params?.filter || null);
     const [committees, setCommittees] = useState<Committee[]>([]);
     const [selectedCommittee, setSelectedCommittee] = useState<string | null>(route.params?.committee || null);
     const [infoVisible, setInfoVisible] = useState(false);
@@ -49,7 +45,7 @@ const Events = ({ navigation }: EventsProps) => {
             setIsLoading(true);
 
             const upcomingEventsData = await getUpcomingEvents();
-            const pastEventsData = await getPastEvents(3, null);
+            const pastEventsData = await getPastEvents(10, null);
 
             const currentTime = new Date();
             const today = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate());
@@ -92,27 +88,6 @@ const Events = ({ navigation }: EventsProps) => {
         fetchCommittees();
     }, [])
 
-    useEffect(() => {
-        if (route.params?.filter !== undefined) {
-            setSelectedFilter(route.params.filter);
-
-            const filterIndex = ["myEvents", "clubWide", ...Object.values(EventType)].indexOf(route.params.filter);
-            if (filterIndex !== -1 && filterScrollViewRef.current) {
-                const scrollPosition = filterIndex * 115;
-                filterScrollViewRef.current.scrollTo({ x: scrollPosition, animated: true });
-            }
-        }
-
-        if (route.params?.committee !== undefined) {
-            setSelectedCommittee(route.params.committee);
-
-            const committeeIndex = committees.findIndex(committee => committee.firebaseDocName === route.params.committee);
-            if (committeeIndex !== -1 && committeeScrollViewRef.current) {
-                const scrollPosition = committeeIndex * 100;
-                committeeScrollViewRef.current.scrollTo({ x: scrollPosition, animated: true });
-            }
-        }
-    }, [route.params, committees]);
 
     useFocusEffect(
         useCallback(() => {
@@ -122,75 +97,6 @@ const Events = ({ navigation }: EventsProps) => {
         }, [hasPrivileges])
     );
 
-    const handleFilterSelect = (filter?: ExtendedEventType, committee?: string) => {
-        // Deselect committee when the same committee is selected
-        if (committee) {
-            if (selectedCommittee === committee) {
-                setSelectedCommittee(null);
-                if (!selectedFilter) {
-                    setSelectedFilter(null);
-                }
-            } else {
-                setSelectedCommittee(committee);
-                if (selectedFilter !== EventType.COMMITTEE_MEETING) {
-                    setSelectedFilter(EventType.COMMITTEE_MEETING);
-                }
-            }
-            return;
-        }
-
-        // Deselect "Committee Meetings" when no committee is selected
-        if (filter === EventType.COMMITTEE_MEETING && selectedFilter === EventType.COMMITTEE_MEETING) {
-            setSelectedFilter(null);
-            setSelectedCommittee(null);
-            return;
-        }
-
-        // Handle other filters
-        if (selectedFilter === filter) {
-            setSelectedFilter(null);
-            setSelectedCommittee(null);
-        } else {
-            setSelectedFilter(filter!);
-            setSelectedCommittee(null);
-        }
-    };
-
-    const filteredEvents = (events: SHPEEvent[]): SHPEEvent[] => {
-        // If no filter is selected, filter out hidden events and committee meetings
-        if (!selectedFilter) {
-            return events.filter(event =>
-                (event.eventType !== EventType.COMMITTEE_MEETING || event.general) &&
-                !event.hiddenEvent
-            );
-        }
-
-        if (selectedFilter === 'myEvents') {
-            return events.filter(event =>
-                (userInfo?.publicInfo?.committees?.includes(event.committee || '') ||
-                    userInfo?.publicInfo?.interests?.includes(event.eventType || '')) &&
-                !event.hiddenEvent
-            );
-        }
-
-        if (selectedFilter === 'clubWide') {
-            return events.filter(event => event.general && !event.hiddenEvent);
-        }
-        // Show hidden events for "Custom Event" filter
-        if (selectedFilter === 'Custom Event') {
-            return events.filter(event => event.eventType === selectedFilter);
-        }
-
-        if (selectedFilter === EventType.COMMITTEE_MEETING) {
-            return events.filter(event =>
-                event.eventType === EventType.COMMITTEE_MEETING &&
-                (selectedCommittee ? event.committee === selectedCommittee : true) &&
-                !event.hiddenEvent
-            );
-        }
-
-        return events.filter(event => event.eventType === selectedFilter && !event.hiddenEvent);
-    };
 
     return (
         <SafeAreaView edges={["top"]} className={`h-full ${darkMode ? "bg-primary-bg-dark" : "bg-primary-bg-light"}`}>
@@ -204,100 +110,6 @@ const Events = ({ navigation }: EventsProps) => {
                     </TouchableOpacity>
                 </View>
 
-                {/* Filters */}
-                <ScrollView
-                    className='pt-3 pb-2'
-                    showsHorizontalScrollIndicator={false}
-                    horizontal={true}
-                    ref={filterScrollViewRef}
-                >
-                    <View className='flex-row px-4 space-x-3'>
-                        <TouchableOpacity
-                            className={`flex-row items-center justify-center rounded-md py-2 px-4 ${darkMode ? "bg-secondary-bg-dark" : "bg-secondary-bg-light"} ${selectedFilter === "myEvents" && 'bg-primary-blue border-primary-blue'}`}
-                            style={{
-                                shadowColor: "#000",
-                                shadowOffset: {
-                                    width: 0,
-                                    height: 2,
-                                },
-                                shadowOpacity: 0.25,
-                                shadowRadius: 3.84,
-
-                                elevation: 5,
-                            }}
-                            onPress={() => handleFilterSelect("myEvents")}
-                        >
-                            <Text className={`font-bold ${selectedFilter === "myEvents" ? 'text-white' : `${darkMode ? "text-white" : "text-black"}`}`}>My Events</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity
-                            className={`flex-row items-center justify-center rounded-md py-2 px-4 ${darkMode ? "bg-secondary-bg-dark" : "bg-secondary-bg-light"} ${selectedFilter === "clubWide" && 'bg-primary-blue border-primary-blue'}`}
-                            style={{
-                                shadowColor: "#000",
-                                shadowOffset: {
-                                    width: 0,
-                                    height: 2,
-                                },
-                                shadowOpacity: 0.25,
-                                shadowRadius: 3.84,
-
-                                elevation: 5,
-                            }}
-                            onPress={() => handleFilterSelect("clubWide")}
-                        >
-                            <Text className={`font-bold ${selectedFilter === "clubWide" ? 'text-white' : `${darkMode ? "text-white" : "text-black"}`}`}>Club Wide</Text>
-                        </TouchableOpacity>
-
-                        {Object.values(EventType).map((type) => (
-                            <TouchableOpacity
-                                key={type}
-                                className={`flex-row items-center justify-center rounded-md py-2 px-4 ${darkMode ? "bg-secondary-bg-dark" : "bg-secondary-bg-light"} ${selectedFilter === type && 'bg-primary-blue border-primary-blue'}`}
-                                style={{
-                                    shadowColor: "#000",
-                                    shadowOffset: {
-                                        width: 0,
-                                        height: 2,
-                                    },
-                                    shadowOpacity: 0.25,
-                                    shadowRadius: 3.84,
-
-                                    elevation: 5,
-                                }}
-                                onPress={() => handleFilterSelect(type)}
-                            >
-                                <Text className={`font-bold ${selectedFilter === type ? 'text-white' : `${darkMode ? "text-white" : "text-black"}`}`}>{type}</Text>
-                            </TouchableOpacity>
-                        ))}
-                    </View>
-                </ScrollView>
-
-                {/* Additional Committee Filter */}
-                {selectedFilter === EventType.COMMITTEE_MEETING && (
-                    <ScrollView horizontal={true} className='pt-3 pb-2' showsHorizontalScrollIndicator={false} ref={committeeScrollViewRef}>
-                        <View className='flex-row px-4 space-x-3'>
-                            {committees.map(committee => (
-                                <TouchableOpacity
-                                    key={committee.firebaseDocName}
-                                    className={`flex-row items-center justify-center rounded-md py-2 px-4 ${darkMode ? "bg-secondary-bg-dark" : "bg-secondary-bg-light"} ${selectedCommittee === committee.firebaseDocName && 'bg-primary-blue border-primary-blue'}`}
-                                    style={{
-                                        shadowColor: "#000",
-                                        shadowOffset: {
-                                            width: 0,
-                                            height: 2,
-                                        },
-                                        shadowOpacity: 0.25,
-                                        shadowRadius: 3.84,
-
-                                        elevation: 5,
-                                    }}
-                                    onPress={() => handleFilterSelect(undefined, committee.firebaseDocName)}
-                                >
-                                    <Text className={`font-bold ${selectedCommittee === committee.firebaseDocName ? 'text-white' : `${darkMode ? "text-white" : "text-black"}`}`}>{committee.name}</Text>
-                                </TouchableOpacity>
-                            ))}
-                        </View>
-                    </ScrollView>
-                )}
 
                 {isLoading &&
                     <View className='mt-10 justify-center items-center'>
@@ -308,17 +120,17 @@ const Events = ({ navigation }: EventsProps) => {
                 {/* Event Listings */}
                 {!isLoading && (
                     <View className='px-4'>
-                        {filteredEvents(todayEvents).length === 0 && filteredEvents(upcomingEvents).length === 0 && filteredEvents(pastEvents).length === 0 ? (
+                        {todayEvents.length === 0 && upcomingEvents.length === 0 && pastEvents.length === 0 ? (
                             <View className='mt-10 justify-center items-center'>
                                 <Text className={`text-lg font-bold ${darkMode ? "text-white" : "text-black"}`}>No Events</Text>
                             </View>
                         ) : (
                             <View>
                                 {/* Today's Events */}
-                                {filteredEvents(todayEvents).length !== 0 && (
+                                {todayEvents.length !== 0 && (
                                     <View className='mt-8'>
                                         <Text className={`mb-3 text-2xl font-bold ${darkMode ? "text-white" : "text-black"}`}>Today's Events</Text>
-                                        {filteredEvents(todayEvents)?.map((event: SHPEEvent, index) => {
+                                        {todayEvents?.map((event: SHPEEvent, index) => {
                                             return (
                                                 <TouchableOpacity
                                                     key={event.id}
@@ -370,10 +182,10 @@ const Events = ({ navigation }: EventsProps) => {
                                 )}
 
                                 {/* Upcoming Events */}
-                                {filteredEvents(upcomingEvents).length !== 0 && (
+                                {upcomingEvents.length !== 0 && (
                                     <View className='mt-8'>
                                         <Text className={`mb-3 text-2xl font-bold ${darkMode ? "text-white" : "text-black"}`}>Upcoming Events</Text>
-                                        {filteredEvents(upcomingEvents)?.map((event: SHPEEvent, index) => {
+                                        {upcomingEvents?.map((event: SHPEEvent, index) => {
                                             return (
                                                 <View key={event.id} className={`${index > 0 && "mt-8"}`}>
                                                     <EventCard event={event} navigation={navigation} />
@@ -384,10 +196,10 @@ const Events = ({ navigation }: EventsProps) => {
                                 )}
 
                                 {/* Past Events */}
-                                {filteredEvents(pastEvents).length !== 0 && (
+                                {pastEvents.length !== 0 && (
                                     <View className='mt-8'>
                                         <Text className={`mb-3 text-2xl font-bold ${darkMode ? "text-white" : "text-black"}`}>Past Events</Text>
-                                        {filteredEvents(pastEvents)?.map((event: SHPEEvent, index) => {
+                                        {pastEvents?.map((event: SHPEEvent, index) => {
                                             return (
                                                 <View key={index} className={`${index > 0 && "mt-8"}`}>
                                                     <EventCard event={event} navigation={navigation} />
@@ -396,11 +208,10 @@ const Events = ({ navigation }: EventsProps) => {
                                         })}
                                     </View>
                                 )}
-                                {!selectedFilter && (
-                                    <TouchableOpacity onPress={() => navigation.navigate("PastEvents")}>
-                                        <Text className='text-xl text-primary-blue mt-8 underline'>View more</Text>
-                                    </TouchableOpacity>
-                                )}
+                                <TouchableOpacity onPress={() => navigation.navigate("PastEvents")}>
+                                    <Text className='text-xl text-primary-blue mt-8 underline'>View more</Text>
+                                </TouchableOpacity>
+
                             </View>
                         )}
                     </View>
