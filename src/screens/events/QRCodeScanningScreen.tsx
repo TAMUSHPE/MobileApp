d70a73cf3be89bc02e18f2deda85f1fbc7f985fe
Josiah Castillo <josiahcastillo03@yahoo.com>
2024-09-24 22:45:23 -0500

    added pinch zoom to QR code scanner

--- src/screens/events/QRCodeScanningScreen.tsx
+++ src/screens/events/QRCodeScanningScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text, TouchableOpacity, Animated, Easing, Dimensions, PixelRatio } from 'react-native';
-import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, TouchableOpacity, Animated, Easing, Dimensions, PixelRatio, Platform } from 'react-native';
+import React, { useEffect, useRef, useState, useMemo, useCallback } from 'react';
 import { CameraView, Camera, BarcodeBounds } from 'expo-camera';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Octicons } from '@expo/vector-icons';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { MainStackParams } from '../../types/navigation';
+import { GestureHandlerRootView, Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
@@ -18,6 +19,11 @@ const QRCodeScanningScreen = ({ navigation }: NativeStackScreenProps<MainStackPa
     const [hasCameraPermissions, setHasCameraPermissions] = useState<boolean | null>(null);
     const [boxColor, setBoxColor] = useState('#FFFFFF');
     const [validScanned, setValidScanned] = useState<boolean>(false);
+    const [zoom, setZoom] = useState(0);
+    const [lastZoom, setLastZoom] = useState(0);
+    
+    const maxZoomFactor = 5;
+    const maxExpoZoom = 0.10;
 
     const pulseAnim = useRef(new Animated.Value(1)).current;
     const boxTop = useRef(new Animated.Value((screenHeight / 2) - 240)).current;
@@ -108,6 +114,53 @@ const QRCodeScanningScreen = ({ navigation }: NativeStackScreenProps<MainStackPa
         }
     };
 
+    const onPinch = useCallback(
+        (event: { velocity: number; scale: number; }) => {
+          const velocity = event.velocity / 20;
+          const outFactor = lastZoom * (Platform.OS === 'ios' ? 40 : 15);
+    
+          let newZoom =
+            velocity > 0
+              ? zoom + event.scale * velocity * (Platform.OS === 'ios' ? 0.01 : 25)
+              : zoom - (event.scale * (outFactor || 1)) * Math.abs(velocity) * (Platform.OS === 'ios' ? 0.02 : 50);
+    
+          if (newZoom < 0) newZoom = 0;
+          else if (newZoom > maxExpoZoom) newZoom = maxExpoZoom;
+    
+          setZoom(newZoom);
+        },
+        [zoom, setZoom, lastZoom, setLastZoom]
+      );
+    
+      const onPinchEnd = useCallback(
+        () => {
+          setLastZoom(zoom);
+        },
+        [zoom, setLastZoom]
+      );
+    
+      const pinchGesture = useMemo(
+        () => Gesture.Pinch().onUpdate(onPinch).onEnd(onPinchEnd),
+        [onPinch, onPinchEnd]
+    );
+
+    const getZoomText = useMemo(() => {
+        const zoomFactor = (zoom / maxExpoZoom) * (maxZoomFactor - 1) + 1;
+        return `${zoomFactor.toFixed(1)}x`;
+    }, [zoom, maxZoomFactor]);
+
+    const resetZoom = () => {
+        const decrementZoom = () => {
+            setZoom(prevZoom => {
+                const newZoom = Math.max(prevZoom - (0.05 * zoom), 0);
+                if (newZoom === 0) clearInterval(interval); 
+                return newZoom;
+            });
+        };
+    
+        const interval = setInterval(decrementZoom, 5); 
+    };
+
     if (hasCameraPermissions === null) {
         return <Text>Requesting for camera permission</Text>;
     }
@@ -125,46 +178,63 @@ const QRCodeScanningScreen = ({ navigation }: NativeStackScreenProps<MainStackPa
                     <Octicons name="x" size={24} color="white" />
                 </TouchableOpacity>
             </View>
-            <CameraView
-                onBarcodeScanned={handleBarCodeScanned}
-                barcodeScannerSettings={{
-                    barcodeTypes: ['qr', 'pdf417'],
-                }}
-                className='flex-1'
-            >
-                {/* Pulsing Effect with Animated Transition */}
-                <Animated.View
-                    style={{
-                        position: 'absolute',
-                        top: boxTop,
-                        left: boxLeft,
-                        width: boxWidth,
-                        height: boxHeight,
-                        transform: [{ scale: pulseAnim }],
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                >
-                    <View className='w-full h-full'>
-                        <View
-                            style={{ borderColor: boxColor }}
-                            className='absolute top-0 left-0 w-[20%] h-[20%] border-t-4 border-l-4 rounded-tl-lg'
-                        />
-                        <View
-                            style={{ borderColor: boxColor }}
-                            className='absolute top-0 right-0 w-[20%] h-[20%] border-t-4 border-r-4 rounded-tr-lg'
-                        />
-                        <View
-                            style={{ borderColor: boxColor }}
-                            className='absolute bottom-0 left-0 w-[20%] h-[20%] border-b-4 border-l-4 rounded-bl-lg'
-                        />
-                        <View
-                            style={{ borderColor: boxColor }}
-                            className='absolute bottom-0 right-0 w-[20%] h-[20%] border-b-4 border-r-4 rounded-br-lg'
-                        />
-                    </View>
-                </Animated.View>
-            </CameraView>
+
+            <GestureHandlerRootView>
+                <GestureDetector gesture={pinchGesture}>
+
+                    <CameraView
+                        onBarcodeScanned={handleBarCodeScanned}
+                        barcodeScannerSettings={{
+                            barcodeTypes: ['qr', 'pdf417'],
+                        }}
+                        className='flex-1'
+                        zoom={zoom}
+                    >
+                        {/* Pulsing Effect with Animated Transition */}
+                        <Animated.View
+                            style={{
+                                position: 'absolute',
+                                top: boxTop,
+                                left: boxLeft,
+                                width: boxWidth,
+                                height: boxHeight,
+                                transform: [{ scale: pulseAnim }],
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <View className='w-full h-full'>
+                                <View
+                                    style={{ borderColor: boxColor }}
+                                    className='absolute top-0 left-0 w-[20%] h-[20%] border-t-4 border-l-4 rounded-tl-lg'
+                                />
+                                <View
+                                    style={{ borderColor: boxColor }}
+                                    className='absolute top-0 right-0 w-[20%] h-[20%] border-t-4 border-r-4 rounded-tr-lg'
+                                />
+                                <View
+                                    style={{ borderColor: boxColor }}
+                                    className='absolute bottom-0 left-0 w-[20%] h-[20%] border-b-4 border-l-4 rounded-bl-lg'
+                                />
+                                <View
+                                    style={{ borderColor: boxColor }}
+                                    className='absolute bottom-0 right-0 w-[20%] h-[20%] border-b-4 border-r-4 rounded-br-lg'
+                                />
+                            </View>
+                        </Animated.View>
+
+                        {/* Circular button for displaying zoom and resetting zoom */}
+                        <TouchableOpacity
+                            onPress={resetZoom}
+                            className="absolute bottom-10 self-center bg-black/50 rounded-full w-12 h-12 flex justify-center items-center"
+                        >
+                            <Text className='text-white text-center font-bold text-base'>{getZoomText}</Text>
+                        </TouchableOpacity>
+
+                    </CameraView>
+                </GestureDetector>
+            </GestureHandlerRootView>
+
 
             <View className='my-2'>
                 <Text className='text-white text-center font-bold text-xl'>Using Scanner</Text>
