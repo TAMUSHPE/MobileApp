2b8c991ad1878705cc87320b2b04ff3a8fb2a153
LucientZ <koblenzpoisiden@gmail.com>
2025-01-07 09:21:47 -0600

    Added jsdoc to hasPrivileges function

--- src/helpers/rolesUtils.ts
+++ src/helpers/rolesUtils.ts
@@ -1,9 +1,22 @@
 import { Roles, User } from "../types/user";
 
+/**
+ * Returns a boolean stating whether or not a user object has the given roles.
+ * 
+ * Note that these are not the same as *claims* and should only be used for frontend things like
+ * accessing certain screens or showing certain buttons.
+ * 
+ * @param   userInfo   The user information object.
+ * @param   roles      The roles to check for the given user.
+ * @returns {boolean}  Whether or not the given `User` object has the specified roles.
+ * 
+ * @example
+ * const isSuperuser = hasPrivileges(user, ['admin', 'developer']);
+ */
 export const hasPrivileges = (
     userInfo: User,
     roles: (keyof Roles)[] = ['admin', 'officer', 'developer', 'lead', 'representative']
-) => {
+): boolean => {
     const userRoles = userInfo?.publicInfo?.roles || {};
     return roles.some(role => userRoles[role]?.valueOf());
 };
