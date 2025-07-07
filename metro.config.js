39e7e93cb4b39c5ef1c5381525e8b455dbfb5a67
Jason <lejason123@gmail.com>
2025-07-06 19:27:04 -0500

    upgrade to expo 53

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,21 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+// metro.config.js
 const { getDefaultConfig } = require("@expo/metro-config");
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  // Existing customization
-  config.resolver.assetExts.push("cjs");
+// Add .cjs support
+config.resolver.assetExts.push("cjs");
 
-  // Setup for react-native-svg-transformer
-  config.transformer = {
-    ...config.transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-  };
-  config.resolver = {
-    ...config.resolver,
-    assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...config.resolver.sourceExts, "svg"],
-  };
+// Setup react-native-svg-transformer safely
+if (!config.transformer) config.transformer = {};
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
 
-  return config;
-})();
+if (!config.resolver) config.resolver = {};
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"];
+
+module.exports = config;
