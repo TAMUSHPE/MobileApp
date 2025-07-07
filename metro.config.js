1d7cb3c7d9d7d872bd368638cd5d6a8bbb8b6fe5
Jason <lejason123@gmail.com>
2025-07-06 19:31:38 -0500

    add metro react native babel transformer package

--- metro.config.js
+++ metro.config.js
@@ -3,19 +3,15 @@ const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// Add .cjs support
 config.resolver.assetExts.push("cjs");
 
-// Setup react-native-svg-transformer safely
-if (!config.transformer) config.transformer = {};
-config.transformer.babelTransformerPath = require.resolve(
-  "react-native-svg-transformer"
-);
-
-if (!config.resolver) config.resolver = {};
 config.resolver.assetExts = config.resolver.assetExts.filter(
   (ext) => ext !== "svg"
 );
-config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"];
+config.resolver.sourceExts.push("svg");
+
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
 
 module.exports = config;
